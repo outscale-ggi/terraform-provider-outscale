3c339fa88308809ceaa8b26f7f55e95856c457a7
lars.smit <lars.smit@oustcale.com>
2020-05-06 22:42:08 +0200

    remove known error

--- qa_tina_tests/USER/FUNCTIONAL/FCU/VPN_Connections/Multi_VPN/test_multi_vpn.py
+++ qa_tina_tests/USER/FUNCTIONAL/FCU/VPN_Connections/Multi_VPN/test_multi_vpn.py
@@ -196,9 +196,7 @@ class Test_multi_vpn(OscTestSuite):
                     timeout=10)
                 assert "1 packets transmitted, 1 received, 0% packet loss" in out
             except OscCommandError:
-                known_error('OPS-11284', 'VPN does not function.')
-                # raise
-            assert False, 'Remove known error code'
+                raise
             try:
                 out, _, _ = SshTools.exec_command_paramiko_2(
                     sshclient2,
