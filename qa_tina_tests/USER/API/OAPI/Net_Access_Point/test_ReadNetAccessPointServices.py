926ffb89f2cf9b4bdfe55e85550113686d30e4fa
taher.kriaa <taher.kriaa@outscale.com>
2020-06-25 09:56:58 +0200

    update

--- qa_tina_tests/USER/API/OAPI/Net_Access_Point/test_ReadNetAccessPointServices.py
+++ qa_tina_tests/USER/API/OAPI/Net_Access_Point/test_ReadNetAccessPointServices.py
@@ -6,7 +6,7 @@ class Test_ReadNetAccessPointServices(OscTestSuite):
 
     def test_T3336_empty_filters(self):
         Services = self.a1_r1.oapi.ReadNetAccessPointServices().response.Services
-        assert len(Services) == 8
+        assert len(Services) == 7
         for service in Services:
             assert (service.ServiceName.split('.')[3]) in Service_Name
             assert service.IpRanges
