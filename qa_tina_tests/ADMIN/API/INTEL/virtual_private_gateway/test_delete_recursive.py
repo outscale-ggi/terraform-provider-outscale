33e3a1bb179dccda14b50648075a4a71cb6b9302
ghassen.gabsi <ghassen.gabsi@outscale.com>
2020-07-10 15:54:32 +0100

    update tests

---
+++ qa_tina_tests/ADMIN/API/INTEL/virtual_private_gateway/test_delete_recursive.py
@@ -0,0 +1,51 @@
+from qa_test_tools.test_base import OscTestSuite
+from qa_tina_tools.tools.tina.create_tools import get_random_public_ip
+from qa_tina_tools.tools.tina.wait_tools import wait_customer_gateways_state, wait_vpn_gateways_state, wait_vpn_connections_state
+from _curses import error
+from qa_sdk_common.exceptions.osc_exceptions import OscApiException
+from qa_test_tools.misc import assert_error
+
+
+class Test_delete_recursive(OscTestSuite):
+
+    @classmethod
+    def setup_class(cls):
+        cls.QUOTAS = {'vpnc_limit': 1}
+        super(Test_delete_recursive, cls).setup_class()
+        cls.cgw_id1 = None
+        cls.vgw_id = None
+        try:
+            res = cls.a1_r1.fcu.CreateCustomerGateway(BgpAsn=65000, IpAddress=get_random_public_ip(), Type='ipsec.1')
+            cls.cgw_id1 = res.response.customerGateway.customerGatewayId
+            wait_customer_gateways_state(cls.a1_r1, [cls.cgw_id1], state='available')
+            res = cls.a1_r1.fcu.CreateVpnGateway(Type='ipsec.1')
+            cls.vgw_id = res.response.vpnGateway.vpnGatewayId
+            wait_vpn_gateways_state(cls.a1_r1, [cls.vgw_id], state='available')
+        except Exception as error:
+            try:
+                cls.teardown_class()
+            except:
+                pass
+            finally:
+                raise error
+
+    @classmethod
+    def teardown_class(cls):
+        super(Test_delete_recursive, cls).teardown_class()
+
+    def test_T5073_recursive_true(self):
+        vpn_connection_id = None
+        try:
+            res = self.a1_r1.fcu.CreateVpnConnection(CustomerGatewayId=self.cgw_id1, Type='ipsec.1', VpnGatewayId=self.vgw_id,
+                                                     Options={'StaticRoutesOnly': True})
+            vpn_connection_id = res.response.vpnConnection.vpnConnectionId
+        finally:
+            if vpn_connection_id:
+                self.a1_r1.intel.vpn.virtual_private_gateway.delete(owner=self.a1_r1.config.account.account_id,
+                                                                    id=vpn_connection_id, recursive=True)
+
+                wait_vpn_connections_state(self.a1_r1, [vpn_connection_id], state='deleted')
+                ret = self.a1_r1.fcu.DescribeVpnGateways()
+                assert not ret.response.vpnGatewaySet
+                ret = self.a1_r1.fcu.DescribeCustomerGateways()
+                assert not ret.response.customerGatewaySet
