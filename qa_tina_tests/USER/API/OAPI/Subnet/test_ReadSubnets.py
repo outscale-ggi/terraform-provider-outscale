3f13b9666c372871314106adbefcb34de35de73f
Ismail Elfadli <ismail.elfadli@MacBook-Pro.local>
2020-07-20 10:38:29 +0200

    add TagFilter test into ReadSubnet call

--- qa_tina_tests/USER/API/OAPI/Subnet/test_ReadSubnets.py
+++ qa_tina_tests/USER/API/OAPI/Subnet/test_ReadSubnets.py
@@ -1,27 +1,44 @@
 import pytest
 
-from qa_test_tools.test_base import OscTestSuite
-from qa_test_tools.misc import assert_dry_run
+from qa_sdk_common.exceptions import OscApiException
+from qa_test_tools.test_base import OscTestSuite, known_error
+from qa_test_tools.misc import assert_dry_run, assert_oapi_error
 from qa_tina_tools.tools.tina.cleanup_tools import cleanup_vpcs
 from qa_tina_tools.tools.tina.wait_tools import wait_vpcs_state
 from qa_test_tools.config.configuration import Configuration
 from qa_tina_tools.specs.check_tools import check_oapi_response
+
+NUM_SUBNETS = 5
+
+def check_tags(subnets, key, value):
+    for subnet in subnets:
+        found = False
+        if hasattr(subnet, 'Tags') and subnet.Tags:
+            for tag in subnet.Tags:
+                if (not key or tag.Key == key) and (not value or tag.Value == value):
+                    found = True
+                    break
+        assert found, 'Could not find the key or value in subnet {}'.format(subnet.SubnetId)
 
 
 class Test_ReadSubnets(OscTestSuite):
 
     @classmethod
     def setup_class(cls):
+
         super(Test_ReadSubnets, cls).setup_class()
+
         cls.net_id = None
         cls.subnet_id = []
         try:
             cls.net_id = cls.a1_r1.oapi.CreateNet(IpRange=Configuration.get('vpc', '10_0_0_0_16')).response.Net.NetId
             wait_vpcs_state(cls.a1_r1, [cls.net_id], state='available')
-            for i in range(2):
+            for i in range(NUM_SUBNETS):
                 cls.subnet_id.append(
                     cls.a1_r1.fcu.CreateSubnet(CidrBlock=Configuration.get('subnet', '10_0_{}_0_24'.format(i + 1)),
                                                VpcId=cls.net_id).response.subnet.subnetId)
+            cls.a1_r1.oapi.CreateTags(ResourceIds=[cls.subnet_id[0]], Tags=[{'Key': 'sub_key', 'Value': 'sub_value'}])
+
         except:
             try:
                 cls.teardown_class()
@@ -41,7 +58,7 @@ class Test_ReadSubnets(OscTestSuite):
 
     def test_T2262_empty_filters(self):
         resp = self.a1_r1.oapi.ReadSubnets().response
-        assert len(resp.Subnets) == 2
+        assert len(resp.Subnets) == NUM_SUBNETS
         check_oapi_response(resp, 'ReadSubnetsResponse')
 
     def test_T2263_valid_params_dry_run(self):
@@ -131,3 +148,43 @@ class Test_ReadSubnets(OscTestSuite):
     def test_T3440_with_other_account_filters(self):
         resp = self.a2_r1.oapi.ReadSubnets(Filters={"NetIds": [self.net_id]}).response
         assert not resp.Subnets
+
+    def test_T5090_with_tags_filter(self):
+        resp = self.a1_r1.oapi.ReadSubnets(Filters={"Tags": ['sub=sub_value']}).response
+        if len(resp.Subnets) == NUM_SUBNETS:
+            known_error('GTW-1420', 'TagFilter on ReadSubnets call not working')
+        assert False, 'Remove the known error code'
+        assert len(resp.Subnets) == 1
+        check_tags(resp.Subnets, 'sub_key', 'sub_value')
+
+    def test_T5091_with_tagkeys_filter(self):
+        resp = self.a1_r1.oapi.ReadSubnets(Filters={"TagKeys": ['sub_key']}).response
+        if len(resp.Subnets) == NUM_SUBNETS:
+            known_error('GTW-1420', 'TagFilter on ReadSubnets call not working')
+        assert False, 'Remove the known error code'
+        assert len(resp.Subnets) == 1
+        check_tags(resp.Subnets, 'sub_key', None)
+
+
+    def test_T5092_with_tagvalues_filter(self):
+        resp = self.a1_r1.oapi.ReadSubnets(Filters={"TagValues": ['sub_value']}).response
+        if len(resp.Subnets) == NUM_SUBNETS:
+            known_error('GTW-1420', 'TagFilter on ReadSubnets call not working')
+        assert False, 'Remove the known error code'
+        assert len(resp.Subnets) == 1
+        check_tags(resp.Subnets, None, 'sub_value')
+
+    def test_T5093_with_invalid_tags_filter(self):
+        resp = self.a1_r1.oapi.ReadSubnets(Filters={"Tags": ['incorrect_sub=incorrect_sub_value']}).response
+        if len(resp.Subnets) == NUM_SUBNETS:
+            known_error('GTW-1420', 'TagFilter on ReadSubnets call not working')
+        assert False, 'Remove the known error code'
+        assert len(resp.Subnets) == 0
+
+    def test_T5094_with_incorrect_tags_filter(self):
+        try:
+            self.a1_r1.oapi.ReadSubnets(Filters={"Tags": 'sub=sub_value'})
+            assert False, 'Call should fail'
+        except OscApiException as error:
+            assert_oapi_error(error, 400, 'TOBECHECKED', 'TOBECHECKED')
+        
