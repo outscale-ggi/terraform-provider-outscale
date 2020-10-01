ee5d8c41d4b6cd013110401800b958bf0669aedf
ghassen.gabsi <ghassen.gabsi@outscale.com>
2020-10-01 14:30:44 +0200

    add error

--- qa_tina_tests/USER/API/OAPI/Snapshot/test_CreateSnapshotExportTask.py
+++ qa_tina_tests/USER/API/OAPI/Snapshot/test_CreateSnapshotExportTask.py
@@ -1,3 +1,5 @@
+from qa_test_tools.test_base import known_error
+
 from qa_tina_tests.USER.API.OAPI.Snapshot.Snapshot import Snapshot
 import pytest
 from qa_tina_tools.tools.tina.wait_tools import wait_snapshots_state
@@ -13,13 +15,16 @@ class Test_CreateSnapshotExportTask(Snapshot):
     def teardown_class(cls):
         super(Test_CreateSnapshotExportTask, cls).teardown_class()
 
-    @pytest.mark.region_synchro_osu
-    @pytest.mark.region_osu
+
     def test_T4675_valid_param(self):
         snapshot_id = self.a1_r1.oapi.CreateSnapshot(VolumeId=self.volume_id1).response.Snapshot.SnapshotId
         wait_snapshots_state(self.a1_r1, [snapshot_id], state='completed')
         try:
-            self.a1_r1.oapi.CreateSnapshotExportTask(SnapshotId=snapshot_id, OsuExport={'DiskImageFormat': 'qcow2', 'OsuBucket':'snap-569'})
+            ret = self.a1_r1.oapi.CreateSnapshotExportTask(SnapshotId=snapshot_id, OsuExport={'DiskImageFormat': 'qcow2', 'OsuBucket':'snap-569'})
+            if not hasattr(ret.response, 'SnapshotExportTask'):
+                known_error('GTW-1488', 'CreateSnapshotExportTask does not return SnapshotExportTask in response')
+            else:
+                assert False, 'Remove known error'
         finally:
             if snapshot_id:
                 self.a1_r1.oapi.DeleteSnapshot(SnapshotId=snapshot_id)
