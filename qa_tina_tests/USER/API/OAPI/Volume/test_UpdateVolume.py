55652eb7104bfe65208e66a8a15017921adc5ea2
ismail.elfadli <ismail.elfadli@outscale.com>
2020-10-09 09:52:52 +0200

    add testlink id for UpdateVolume tests

--- qa_tina_tests/USER/API/OAPI/Volume/test_UpdateVolume.py
+++ qa_tina_tests/USER/API/OAPI/Volume/test_UpdateVolume.py
@@ -44,58 +44,58 @@ class Test_UpdateVolume(OscTestSuite):
         finally:
             super(Test_UpdateVolume, self).teardown_method(method)
 
-    def test_TXXX_valid_params(self):
+    def test_T5232_valid_params(self):
         resp = self.a1_r1.oapi.UpdateVolume(VolumeId=self.vol.VolumeId, Size=5).response
         compare_validate_volumes(self.vol, resp.Volume, Size=5)
 
-    def test_TXXX_valid_params_dry_run(self):
-        resp = self.a1_r1.oapi.UpdateVolume(VolumeId=self.vol.VolumeId, Size=5, DryRun=True)
-        assert_dry_run(resp)
-
-    def test_TXXX_dry_run_false(self):
-        resp = self.a1_r1.oapi.UpdateVolume(VolumeId=self.vol.VolumeId, Size=5, DryRun=False)
-        assert_dry_run(resp)
-
-    def test_TXXX_without_params(self):
+    def test_T5233_without_params(self):
         try:
             self.a1_r1.oapi.UpdateVolume()
             assert False, 'Call should not have been successful, missing parameter'
         except OscApiException as error:
             assert_oapi_error(error, 400, 'MissingParameter', '7000')
 
-    def test_TXXX_without_size_volume(self):
+    def test_T5234_with_dry_run(self):
+        resp = self.a1_r1.oapi.UpdateVolume(VolumeId=self.vol.VolumeId, Size=5, DryRun=True)
+        assert_dry_run(resp)
+
+    def test_T5235_with_dry_run_false(self):
+        resp = self.a1_r1.oapi.UpdateVolume(VolumeId=self.vol.VolumeId, Size=5, DryRun=False)
+        assert_dry_run(resp)
+
+    def test_T5236_without_size_volume(self):
         resp = self.a1_r1.oapi.UpdateVolume(VolumeId=self.vol.VolumeId).response.Volume
         compare_validate_volumes(self.vol, resp.Volume)
 
-    def test_TXXX_without_volume_id(self):
+    def test_T5237_without_volume_id(self):
         try:
             self.a1_r1.oapi.UpdateVolume(Size=5)
             assert False, 'Call should not have been successful, missing parameter'
         except OscApiException as error:
             assert_oapi_error(error, 400, 'MissingParameter', '7000')
 
-    def test_TXXX_with_invalid_size(self):
+    def test_T5238_with_invalid_size(self):
         try:
             self.a1_r1.oapi.UpdateVolume(VolumeId=self.vol.VolumeId, Size='foo')
             assert False
         except OscApiException as error:
             assert_oapi_error(error, 400, 'InvalidParameterValue', '4045')
 
-    def test_TXXX_with_invalid_vol_id(self):
+    def test_T5239_with_invalid_vol_id(self):
         try:
             self.a1_r1.oapi.UpdateVolume(VolumeId='foo', Size=5)
             assert False
         except OscApiException as error:
             assert_oapi_error(error, 400, 'InvalidParameterValue', '4045')
 
-    def test_TXXX_with_size_max(self):
+    def test_T5240_with_size_max(self):
         try:
             self.a1_r1.oapi.UpdateVolume(VolumeId=self.vol.VolumeId, Size=20000)
             assert False
         except OscApiException as error:
             assert_oapi_error(error, 400, 'InvalidParameterValue', '4045')
 
-    def test_TXXX_with_size_min(self):
+    def test_T5241_with_size_min(self):
         try:
             self.a1_r1.oapi.UpdateVolume(VolumeId=self.vol.VolumeId, Size=1)
             assert False
@@ -103,7 +103,7 @@ class Test_UpdateVolume(OscTestSuite):
             assert_oapi_error(error, 400, 'InvalidParameterValue', '4045')
 
     @pytest.mark.tag_sec_confidentiality
-    def test_TXXX_from_another_account(self):
+    def test_T5242_from_another_account(self):
         try:
             self.a2_r1.oapi.UpdateVolume(VolumeId=self.vol.VolumeId, Size=5)
             assert False, 'Call should not have been successful'
