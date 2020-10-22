aebe5869e36f2ec54859518ad302a22684f01a88
ismail.elfadli <ismail.elfadli@outscale.com>
2020-10-22 14:06:13 +0200

    update UpdateVolume API and Functional

--- qa_tina_tests/USER/FUNCTIONAL/OAPI/Volume/test_UpdateVolume.py
+++ qa_tina_tests/USER/FUNCTIONAL/OAPI/Volume/test_UpdateVolume.py
@@ -6,7 +6,6 @@ from qa_common_tools.ssh import SshTools
 from qa_test_tools.test_base import OscTestSuite
 
 from qa_test_tools.config import config_constants as constants
-from qa_tina_tests.ADMIN.FUNCTIONAL.streaming import write_on_device
 from qa_tina_tools.tina.check_tools import format_mount_volume, create_text_file_volume, read_text_file_volume, \
     umount_volume
 from qa_tina_tools.tools.tina.create_tools import create_instances
@@ -52,12 +51,6 @@ class Test_UpdateVolume(OscTestSuite):
                                                                 self.inst_info[KEY_PAIR][PATH],
                                                                 username=self.a1_r1.config.region.get_info(
                                                                     constants.CENTOS_USER))
-            if self.with_fio:
-                cmd = 'sudo yum install -y epel-release'
-                SshTools.exec_command_paramiko_2(self.sshclient, cmd)
-                cmd = 'sudo yum install -y fio'
-                SshTools.exec_command_paramiko_2(self.sshclient, cmd)
-
             self.device = '/dev/xvdc'
             self.volume_mount = '/mnt/vol'
             format_mount_volume(self.sshclient, self.device, self.volume_mount, True)
@@ -92,8 +85,8 @@ class Test_UpdateVolume(OscTestSuite):
             wait_volumes_state(self.a1_r1, [self.vol_id], state='available')
             self.is_attached = False
 
-            #resp = self.a1_r1.oapi.UpdateVolume(VolumeId=self.vol_id, Size=5).response
-            #wait_volumes_state(self.a1_r1, [self.vol_id], state='available')
+            self.a1_r1.oapi.UpdateVolume(VolumeId=self.vol_id, Size=5)
+            wait_volumes_state(self.a1_r1, [self.vol_id], state='available')
 
             self.ret_link = self.a1_r1.oapi.LinkVolume(VolumeId=self.vol_id, VmId=self.inst_info[INSTANCE_ID_LIST][0],
                                                        DeviceName='/dev/xvdc')
@@ -110,11 +103,34 @@ class Test_UpdateVolume(OscTestSuite):
             pytest.fail("An unexpected error happened : " + str(error))
 
     def test_real_volume_size(self):
-        # resp = self.a1_r1.oapi.UpdateVolume(VolumeId=self.vol_id, Size=5).response
+        umount_volume(self.sshclient, self.volume_mount)
+
+        self.a1_r1.oapi.UnlinkVolume(VolumeId=self.vol_id)
+        wait_volumes_state(self.a1_r1, [self.vol_id], state='available')
+        self.is_attached = False
+
+        self.a1_r1.oapi.UpdateVolume(VolumeId=self.vol_id, Size=5)
         wait_volumes_state(self.a1_r1, [self.vol_id], state='available')
         self.ret_link = self.a1_r1.oapi.LinkVolume(VolumeId=self.vol_id, VmId=self.inst_info[INSTANCE_ID_LIST][0],
                                                    DeviceName='/dev/xvdc')
         wait_volumes_state(self.a1_r1, [self.vol_id], state='in-use')
-        md5sum = write_on_device(sshclient=self.sshclient, device=self.device, folder=self.volume_mount, f_num=1,
-                                 size=10**9, with_md5sum=self.with_md5sum, with_fio=self.with_fio)
+        self.is_attached = True
+
+        cmd = 'sudo mount -o nouuid {} {}'.format(self.device, self.volume_mount)
+        SshTools.exec_command_paramiko_2(self.sshclient, cmd)
+
+        # extend volume
+        cmd = 'sudo xfs_growfs {}'.format(self.volume_mount)
+        SshTools.exec_command_paramiko_2(self.sshclient, cmd)
+
+        # write file
+        cmd = 'sudo openssl rand -out {}/data_x.txt -base64 $(({} * 2**20 * 3/4))'.format(self.volume_mount, 10**9)
+        SshTools.exec_command_paramiko_2(self.sshclient, cmd, eof_time_out=500)
+
+        cmd = 'sudo cat {}/data_*.txt | md5sum'.format(self.volume_mount)
+        out, _, _ = SshTools.exec_command_paramiko_2(self.sshclient, cmd, eof_time_out=500)
+        md5sum = out.split(' ')[0]
+
+        umount_volume(self.sshclient, self.volume_mount)
+
         assert md5sum
