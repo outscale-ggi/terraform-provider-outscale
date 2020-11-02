b9cd4d6d1da3aa92869bd567751ae19dca17700e
ismail.elfadli <ismail.elfadli@outscale.com>
2020-11-02 16:07:01 +0100

    add known error to UpdateVolume API

--- qa_tina_tests/USER/API/OAPI/Volume/test_UpdateVolume.py
+++ qa_tina_tests/USER/API/OAPI/Volume/test_UpdateVolume.py
@@ -3,7 +3,7 @@ import pytest
 from qa_sdk_common.exceptions import OscApiException
 from qa_test_tools.config import config_constants as constants
 from qa_test_tools.misc import assert_oapi_error, assert_dry_run, assert_error
-from qa_test_tools.test_base import OscTestSuite
+from qa_test_tools.test_base import OscTestSuite, known_error
 from qa_tina_tools.specs.check_tools import check_oapi_response
 from qa_tina_tools.tools.tina.create_tools import create_instances
 from qa_tina_tools.tools.tina.delete_tools import delete_instances
@@ -51,7 +51,10 @@ class Test_UpdateVolume(OscTestSuite):
 
     def test_T5232_valid_params(self):
         resp = self.a1_r1.oapi.UpdateVolume(VolumeId=self.vol.VolumeId, Size=5).response
+        #wait_volumes_state(self.a1_r1, [self.vol.VolumeId], state='completed')
         check_oapi_response(resp, 'UpdateVolumeResponse')
+        if resp.Volume.Size != 5:
+            known_error("TINA-5994", "waiting for product decision")
         compare_validate_volumes(self.vol, resp.Volume, Size=5)
 
     def test_T5233_without_params(self):
@@ -121,8 +124,9 @@ class Test_UpdateVolume(OscTestSuite):
             assert_oapi_error(error, 400, 'InvalidParameterValue', '4045')
 
     def test_T5245_with_too_small(self):
-        self.a1_r1.oapi.UpdateVolume(VolumeId=self.vol.VolumeId, Size=0)
-
+        ret = self.a1_r1.oapi.UpdateVolume(VolumeId=self.vol.VolumeId, Size=0)
+        if ret.status_code == 200:
+            known_error("TINA-5996", "UpdateVolume success with a size 0, waiting for product decision")
 
     def test_T5241_with_lower_size(self):
         try:
