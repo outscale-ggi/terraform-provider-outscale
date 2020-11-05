3b1413fbd32719c25fafd6e7b6f00a85f43ed7fd
ismail.elfadli <ismail.elfadli@outscale.com>
2020-11-05 10:41:39 +0100

    update ReadLoadBalancer

--- qa_tina_tests/USER/API/OAPI/LoadBalancer/test_ReadLoadBalancers.py
+++ qa_tina_tests/USER/API/OAPI/LoadBalancer/test_ReadLoadBalancers.py
@@ -177,9 +177,10 @@ class Test_ReadLoadBalancers(LoadBalancer):
             assert ret[0].LoadBalancerStickyCookiePolicies[0].PolicyName == policy_name_lb
             assert ret[0].LoadBalancerStickyCookiePolicies[1].PolicyName == policy_name_lb2
         finally:
-            if lb_policy and lb_policy2:
-                try:
+            try:
+                if lb_policy:
                     self.a1_r1.oapi.DeleteLoadBalancerPolicy(PolicyName=policy_name_lb)
+                if lb_policy2:
                     self.a1_r1.oapi.DeleteLoadBalancerPolicy(PolicyName=policy_name_lb2)
-                except:
-                    pass
+            finally:
+                pass
