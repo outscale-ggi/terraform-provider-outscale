4119d8b44d1d30b83b519c4d8cd75a86852ba44e
ismail.elfadli <ismail.elfadli@outscale.com>
2020-11-26 12:15:38 +0100

    update accounting update volume better 2

--- qa_tina_tests/ADMIN/FUNCTIONAL/accounting/test_update_volume.py
+++ qa_tina_tests/ADMIN/FUNCTIONAL/accounting/test_update_volume.py
@@ -1,10 +1,9 @@
 import datetime
-
+from time import sleep
 from qa_test_tools.test_base import OscTestSuite
 from qa_tina_tools.tools.tina.wait_tools import wait_volumes_state
 
-ACCOUNTING_DELTA = 2
-
+ACCOUNTING_DELTA = 30
 class Test_update_volume(OscTestSuite):
 
     @classmethod
@@ -25,20 +24,23 @@ class Test_update_volume(OscTestSuite):
         try:
             self.vol = self.a1_r1.oapi.CreateVolume(Size=initial_size, SubregionName=self.azs[0],
                                                     Iops=iops, VolumeType='io1').response.Volume
-            wait_volumes_state(self.a1_r1, [self.vol.VolumeId], state='available')
             dates.append(datetime.datetime.utcnow())
+            wait_volumes_state(self.a1_r1, [self.vol.VolumeId], state='available')
+            sleep(60)
 
             self.a1_r1.oapi.UpdateVolume(VolumeId=self.vol.VolumeId, Size=update_size)
-            wait_volumes_state(self.a1_r1, [self.vol.VolumeId], state='available')
             dates.append(datetime.datetime.utcnow())
+            wait_volumes_state(self.a1_r1, [self.vol.VolumeId], state='available')
+            sleep(120)
 
             self.a1_r1.oapi.DeleteVolume(VolumeId=self.vol.VolumeId)
-            wait_volumes_state(osc_sdk=self.a1_r1, cleanup=True, volume_id_list=[self.vol.VolumeId])
             dates.append(datetime.datetime.utcnow())
+            wait_volumes_state(osc_sdk=self.a1_r1, cleanup=True, volume_id_list=[self.vol.VolumeId])
             is_deleted = True
+            sleep(60)
 
             user_name = self.a1_r1.config.account.account_id
-            ret = self.a1_r1.intel.accounting.find(owner=[user_name], operation='CreateVolume', limit=6,
+            ret = self.a1_r1.intel.accounting.find(owner=[user_name], orders=[('id', 'ASC')], operation='CreateVolume', limit=6,
                                                    instance=self.vol.VolumeId).response.result.results
             assert len(ret) == 6
             i = 0
@@ -46,7 +48,7 @@ class Test_update_volume(OscTestSuite):
             """
                 i = 0,1 CreateVolume (0: Opening the Size event 5Go, 1: for IOPS) <-> j : 0
                 i = 2,3 UpdateVolume (2: Closing the previous Size, 3: Update 10Go) <-> j : 1
-                i = 4,5 DeleteVolume (4: Closing the Size, 5: for IOPS) <-> j reste sur 2
+                i = 4,5 DeleteVolume (4: Closing the Size, 5: for IOPS) <-> j : 2
             """
             for r in ret:
                 assert r.is_correlated is True
@@ -54,7 +56,7 @@ class Test_update_volume(OscTestSuite):
                 assert (r.type == 'BSU:VolumeIOPS:io1' if i in [1, 5] else r.type == 'BSU:VolumeUsage:io1')
                 assert r.instance == self.vol.VolumeId
                 assert abs(dates[j] - r.created.dt) <= \
-                       datetime.timedelta(minutes=1)
+                       datetime.timedelta(seconds=ACCOUNTING_DELTA)
 
                 if i in [0, 1, 3]:
                     assert(r.closing) is False
@@ -70,7 +72,7 @@ class Test_update_volume(OscTestSuite):
                     assert(r.closing) is True
                     assert(r.is_last) is True
 
-                i = i+1
+                i += 1
                 j = (i-j)-1
         finally:
             if not is_deleted:
