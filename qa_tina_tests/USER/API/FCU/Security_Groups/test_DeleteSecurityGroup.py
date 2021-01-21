5c5c8358d089f66d7548a7115f55b6197dca30fb
taher.kriaa <taher.kriaa@outscale.com>
2021-01-21 11:41:12 +0100

    new tests for DeleteSecurityGroup

--- qa_tina_tests/USER/API/FCU/Security_Groups/test_DeleteSecurityGroup.py
+++ qa_tina_tests/USER/API/FCU/Security_Groups/test_DeleteSecurityGroup.py
@@ -2,8 +2,15 @@ import re
 
 from qa_sdk_common.exceptions.osc_exceptions import OscApiException
 from qa_test_tools.config.configuration import Configuration
-from qa_test_tools.misc import assert_error
+from qa_test_tools.misc import assert_error, id_generator
 from qa_test_tools.test_base import OscTestSuite
+from qa_tina_tools.tools.tina.create_tools import create_instances, \
+    create_load_balancer, create_vpc
+from qa_tina_tools.tools.tina.delete_tools import delete_instances
+from qa_tina_tools.tina.info_keys import SECURITY_GROUP_ID, SUBNETS, SUBNET_ID
+from qa_tina_tools.tools.tina.cleanup_tools import cleanup_vpcs
+from qa_tina_tools.tools.tina.info_keys import VPC_ID
+from qa_test_tools import misc
 
 
 class Test_DeleteSecurityGroup(OscTestSuite):
@@ -15,10 +22,23 @@ class Test_DeleteSecurityGroup(OscTestSuite):
     @classmethod
     def setup_class(cls):
         super(Test_DeleteSecurityGroup, cls).setup_class()
+        cls.instance_info_a1 = None
+        try:
+            cls.instance_info_a1 = create_instances(cls.a1_r1)
+        except Exception as error:
+            try:
+                cls.teardown_class()
+            except:
+                pass
+            raise error
 
     @classmethod
     def teardown_class(cls):
-        super(Test_DeleteSecurityGroup, cls).teardown_class()
+        try:
+            if cls.instance_info_a1:
+                delete_instances(cls.a1_r1, cls.instance_info_a1)
+        finally:
+            super(Test_DeleteSecurityGroup, cls).teardown_class()
 
     def test_T981_using_id(self):
         sg_name = 'test_sg'
@@ -56,7 +76,7 @@ class Test_DeleteSecurityGroup(OscTestSuite):
         sg_id = None
         try:
             vpc_id = self.a1_r1.fcu.CreateVpc(CidrBlock=Configuration.get('vpc', '10_0_0_0_16')).response.vpc.vpcId
-            sg_id = self.a1_r1.fcu.DescribeSecurityGroups(Filter=[{'Name' : 'vpc-id', 'Value': vpc_id}]).response.securityGroupInfo[0].groupId
+            sg_id = self.a1_r1.fcu.DescribeSecurityGroups(Filter=[{'Name': 'vpc-id', 'Value': vpc_id}]).response.securityGroupInfo[0].groupId
             self.a1_r1.fcu.DeleteSecurityGroup(GroupId=sg_id)
             assert False, 'Call should not have been successful'
         except OscApiException as error:
@@ -67,3 +87,27 @@ class Test_DeleteSecurityGroup(OscTestSuite):
                     self.a1_r1.fcu.DeleteVpc(VpcId=vpc_id)
                 except:
                     pass
+
+    def test_T5459_associated_with_vm(self):
+        try:
+            self.a1_r1.fcu.DeleteSecurityGroup(GroupId=self.instance_info_a1[SECURITY_GROUP_ID])
+        except OscApiException as error:
+            assert_error(error, 400, "InvalidGroup.InUse", "There are active Instances or NetworkInterfaces using the security group: {}"
+                         .format(self.instance_info_a1[SECURITY_GROUP_ID]))
+
+    def test_T5460_associated_with_lbu(self):
+        vpc_info = create_vpc(self.a1_r1)
+        ret = self.a1_r1.fcu.CreateSecurityGroup(GroupDescription='test', GroupName=misc.id_generator(prefix='sg'), VpcId=vpc_info[VPC_ID])
+        sg_id = ret.response.groupId
+        lb_name = id_generator(prefix='lbu-')
+        create_load_balancer(self.a1_r1, lb_name, listeners=[{'InstancePort': 65535, 'Protocol': 'HTTP', 'LoadBalancerPort': 80}],
+                              sg=[sg_id], subnets=[vpc_info[SUBNETS][0][SUBNET_ID]])
+        lb_name = lb_name
+        try:
+            self.a1_r1.fcu.DeleteSecurityGroup(GroupId=sg_id)
+        except OscApiException as error:
+            assert_error(error, 400, "InvalidGroup.InUse", "There are active Instances or NetworkInterfaces using the security group: {}"
+                         .format(sg_id))
+        finally:
+            if vpc_info:
+                cleanup_vpcs(self.a1_r1, vpc_id_list=vpc_info[VPC_ID], force=True)
