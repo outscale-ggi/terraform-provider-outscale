e0c15d92c8aec12798b73a197c3773a93eae0bc6
ghassen.gabsi <ghassen.gabsi@outscale.com>
2021-02-19 10:11:37 +0100

    updates and add test perf oos

--- qa_tina_tests/USER/PERF/put_object_marine.py
+++ qa_tina_tests/USER/PERF/put_object_marine.py
@@ -1,7 +1,8 @@
 from qa_tina_tests.USER.PERF.perf_objects import func_objects
 
 
-def perf_multipart_upload_marine(oscsdk, logger, queue, args):
+def put_object_marine(oscsdk, logger, queue, args):
     result = {'status': 'OK'}
-    func_objects(oscsdk=oscsdk, func='put_object', service='oos', logger=logger, size='30k', result=result)
+    print(args)
+    func_objects(oscsdk=oscsdk, func='put_object', service='oos', logger=logger, size='10k', result=result)
     queue.put(result.copy())
