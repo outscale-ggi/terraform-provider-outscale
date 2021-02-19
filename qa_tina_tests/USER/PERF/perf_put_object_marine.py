96db2b532b45d6bf12064d64c8445b7b379e5d98
ghassen.gabsi <ghassen.gabsi@outscale.com>
2021-02-19 14:28:25 +0100

    add test perf for marine

--- qa_tina_tests/USER/PERF/perf_put_object_marine.py
+++ qa_tina_tests/USER/PERF/perf_put_object_marine.py
@@ -1,7 +1,7 @@
 from qa_tina_tests.USER.PERF.perf_objects import func_objects
 
 
-def put_object_marine(oscsdk, logger, queue, args):
+def perf_put_object_marine(oscsdk, logger, queue, args):
     result = {'status': 'OK'}
     print(args)
     func_objects(oscsdk=oscsdk, func='put_object', service='oos', logger=logger, size='10k', result=result)
