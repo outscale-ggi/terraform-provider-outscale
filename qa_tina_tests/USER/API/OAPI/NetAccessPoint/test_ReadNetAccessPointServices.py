483f716f04300acce313e353a78538a40b9c24a2
lars.smit <lars.smit@outscale.com>
2021-03-04 18:33:06 +0100

    revert

--- qa_tina_tests/USER/API/OAPI/NetAccessPoint/test_ReadNetAccessPointServices.py
+++ qa_tina_tests/USER/API/OAPI/NetAccessPoint/test_ReadNetAccessPointServices.py
@@ -3,19 +3,25 @@ from qa_test_tools.test_base import OscTestSuite
 
 Service_Name = ['fcu', 'lbu', 'eim', 'icu', 'directlink', 'api', 'kms', 'oos']
 
+<<<<<<< Upstream, based on origin/TINA-2.5.17
 SERVICE_NAMES = ['fcu', 'lbu', 'eim', 'icu', 'directlink', 'api', 'kms']
+=======
+>>>>>>> 078b246 revert
 
 class Test_ReadNetAccessPointServices(OscTestSuite):
 
     def test_T3336_empty_filters(self):
         services = self.a1_r1.oapi.ReadNetAccessPointServices().response.Services
         assert len(services) == len(Service_Name)
+<<<<<<< Upstream, based on origin/TINA-2.5.17
         for service in services:
             assert service.ServiceName.split('.')[3] in Service_Name
         services = self.a1_r1.oapi.ReadNetAccessPointServices().response.Services
         assert len(services) == 7
+=======
+>>>>>>> 078b246 revert
         for service in services:
-            assert service.ServiceName.split('.')[3] in SERVICE_NAMES
+            assert service.ServiceName.split('.')[3] in Service_Name
             assert service.IpRanges
             assert service.ServiceName
             assert service.ServiceId
