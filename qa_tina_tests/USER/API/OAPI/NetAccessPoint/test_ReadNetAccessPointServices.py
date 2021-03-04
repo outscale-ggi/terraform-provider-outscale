5e54bb77ce2ecfbd35798f2f184f39c3c31b5380
ismail.elfadli <ismail.elfadli@outscale.com>
2021-03-04 13:06:29 +0100

    update ReadNetAccessPointServices test (better pipeline)

--- qa_tina_tests/USER/API/OAPI/NetAccessPoint/test_ReadNetAccessPointServices.py
+++ qa_tina_tests/USER/API/OAPI/NetAccessPoint/test_ReadNetAccessPointServices.py
@@ -10,7 +10,7 @@ class Test_ReadNetAccessPointServices(OscTestSuite):
         services = self.a1_r1.oapi.ReadNetAccessPointServices().response.Services
         assert len(services) == len(Service_Name)
         for service in services:
-            assert (service.ServiceName.split('.')[3]) in Service_Name
+            assert service.ServiceName.split('.')[3] in Service_Name
             assert service.IpRanges
             assert service.ServiceName
             assert service.ServiceId
