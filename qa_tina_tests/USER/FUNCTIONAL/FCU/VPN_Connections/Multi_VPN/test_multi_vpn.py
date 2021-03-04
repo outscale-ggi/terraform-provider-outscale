6da18f208a29761836aaf3868725740c8aa22851
lars.smit <lars.smit@outscale.com>
2021-03-04 13:02:00 +0100

    modified test to have it sequential

--- qa_tina_tests/USER/FUNCTIONAL/FCU/VPN_Connections/Multi_VPN/test_multi_vpn.py
+++ qa_tina_tests/USER/FUNCTIONAL/FCU/VPN_Connections/Multi_VPN/test_multi_vpn.py
@@ -96,6 +96,10 @@ class Test_multi_vpn(OscTestSuite):
         self.vpc_info = create_vpc(osc_sdk=self.a1_r1, nb_instance=1, default_rtb=default_rtb)
 
         self.a1_r1.fcu.AttachVpnGateway(VpcId=self.vpc_info[VPC_ID], VpnGatewayId=self.vgw_id)        
+<<<<<<< Upstream, based on origin/TINA-2.5.17
+=======
+    
+>>>>>>> af551ee modified test to have it sequential
 
         rtb_id = None
         if default_rtb:
@@ -149,6 +153,7 @@ class Test_multi_vpn(OscTestSuite):
                                                             username=self.a1_r1.config.region.get_info(constants.CENTOS_USER))
 
             setup_customer_gateway(self.a1_r1, sshclient1, self.vpc_info[SUBNETS][0][INSTANCE_SET][0]['privateIpAddress'],
+<<<<<<< Upstream, based on origin/TINA-2.5.17
                                    self.inst_cgw1_info, vgw1_ip, psk1_key, static, vpn1_id, racoon=racoon)
 
             # wait vpc instance state == ready before try to make ping
@@ -176,6 +181,38 @@ class Test_multi_vpn(OscTestSuite):
                     ret = self.a1_r1.fcu.DescribeVpnConnections(VpnConnectionId=[vpn1_id])
                     self.logger.info('state = %s'.format(ret.response.vpnConnectionSet[0].state))
                     self.logger.info('telemetry = %s'.format(ret.response.vpnConnectionSet[0].vgwTelemetry[0].status))
+=======
+                                   self.inst_cgw1_info, vgw1_ip, psk1_key, static, vpn1_id,racoon=racoon)
+
+            # wait vpc instance state == ready before try to make ping
+            wait_tools.wait_instances_state(self.a1_r1,
+                                 [self.vpc_info[SUBNETS][0][INSTANCE_ID_LIST][0]],
+                                 state='ready')
+
+            inst1 = self.inst_cgw1_info[INSTANCE_SET][0]
+            inst_vpc = self.vpc_info[SUBNETS][0][INSTANCE_SET][0]
+            self.logger.info("inst1 cgw -> : {} -- {}".format(inst1['ipAddress'], inst1['privateIpAddress']))
+            self.logger.info("inst vpc -> : None -- {}".format(inst_vpc['privateIpAddress']))
+
+            # try to make ping from CGW to VPC instance
+            try:
+                out, _, _ = SshTools.exec_command_paramiko(
+                    sshclient1,
+                    'ping -I {} -W 1 -c 1 {}'.format(inst1['privateIpAddress'], inst_vpc['privateIpAddress']),
+                    retry=20,
+                    timeout=10)
+                assert "1 packets transmitted, 1 received, 0% packet loss" in out
+            except OscCommandError:
+                raise
+
+            # check vpn connection status
+            start = datetime.now()
+            while (datetime.now() - start).total_seconds() < 60:
+                try:
+                    ret = self.a1_r1.fcu.DescribeVpnConnections(VpnConnectionId=[vpn1_id])
+                    self.logger.info('state = {}'.format(ret.response.vpnConnectionSet[0].state))
+                    self.logger.info('telemetry = {}'.format(ret.response.vpnConnectionSet[0].vgwTelemetry[0].status))
+>>>>>>> af551ee modified test to have it sequential
                     assert ret.response.vpnConnectionSet[0].state == 'available'
                     assert ret.response.vpnConnectionSet[0].vgwTelemetry[0].status == 'UP'
                     break
@@ -214,6 +251,10 @@ class Test_multi_vpn(OscTestSuite):
                                                          ToPort=-1,
                                                          CidrIp="{}/32".format(self.inst_cgw2_info[INSTANCE_SET][0]['privateIpAddress']))
 
+<<<<<<< Upstream, based on origin/TINA-2.5.17
+=======
+
+>>>>>>> af551ee modified test to have it sequential
             # wait CGW state == ready before making configuration
             wait_tools.wait_instances_state(self.a1_r1, [self.inst_cgw2_info[INSTANCE_ID_LIST][0]], state='ready')
 
@@ -223,11 +264,21 @@ class Test_multi_vpn(OscTestSuite):
             setup_customer_gateway(self.a1_r1, sshclient2, self.vpc_info[SUBNETS][0][INSTANCE_SET][0]['privateIpAddress'],
                                    self.inst_cgw2_info, vgw2_ip, psk2_key, static, vpn2_id, index=1, racoon=racoon)
 
+<<<<<<< Upstream, based on origin/TINA-2.5.17
+=======
+
+>>>>>>> af551ee modified test to have it sequential
             inst2 = self.inst_cgw2_info[INSTANCE_SET][0]
+<<<<<<< Upstream, based on origin/TINA-2.5.17
             self.logger.info("inst2 cgw -> : %s -- %s".format(inst2['ipAddress'], inst2['privateIpAddress']))
             self.logger.info("inst vpc -> : None -- %s".format(inst_vpc['privateIpAddress']))
+=======
+            self.logger.info("inst2 cgw -> : {} -- {}".format(inst2['ipAddress'], inst2['privateIpAddress']))
+            self.logger.info("inst vpc -> : None -- {}".format(inst_vpc['privateIpAddress']))
+>>>>>>> af551ee modified test to have it sequential
 
             # try to make ping from CGW to VPC instance
+<<<<<<< Upstream, based on origin/TINA-2.5.17
             out, _, _ = SshTools.exec_command_paramiko(
                 sshclient2,
                 'ping -I {} -W 1 -c 1 {}'.format(inst2['privateIpAddress'], inst_vpc['privateIpAddress']),
@@ -256,6 +307,42 @@ class Test_multi_vpn(OscTestSuite):
                 retry=20,
                 timeout=10)
             assert "1 packets transmitted, 1 received, 0% packet loss" in out
+=======
+            try:
+                out, _, _ = SshTools.exec_command_paramiko(
+                    sshclient2,
+                    'ping -I {} -W 1 -c 1 {}'.format(inst2['privateIpAddress'], inst_vpc['privateIpAddress']),
+                    retry=20,
+                    timeout=10)
+                assert "1 packets transmitted, 1 received, 0% packet loss" in out
+            except OscCommandError:
+                raise
+
+            # check vpn connection status
+            start = datetime.now()
+            while (datetime.now() - start).total_seconds() < 60:
+                try:
+                    ret = self.a1_r1.fcu.DescribeVpnConnections(VpnConnectionId=[vpn2_id])
+                    self.logger.info('state = {}'.format(ret.response.vpnConnectionSet[0].state))
+                    self.logger.info('telemetry = {}'.format(ret.response.vpnConnectionSet[0].vgwTelemetry[0].status))
+                    assert ret.response.vpnConnectionSet[0].state == 'available'
+                    assert ret.response.vpnConnectionSet[0].vgwTelemetry[0].status == 'UP'
+                    break
+                except Exception:
+                    time.sleep(5)
+                    pass
+
+            # try to make ping from CGW to VPC instance
+            try:
+                out, _, _ = SshTools.exec_command_paramiko(
+                    sshclient1,
+                    'ping -I {} -W 1 -c 1 {}'.format(inst1['privateIpAddress'], inst_vpc['privateIpAddress']),
+                    retry=20,
+                    timeout=10)
+                assert "1 packets transmitted, 1 received, 0% packet loss" in out
+            except OscCommandError:
+                raise
+>>>>>>> af551ee modified test to have it sequential
 
             # check vpn connection status
             start = datetime.now()
@@ -281,6 +368,7 @@ class Test_multi_vpn(OscTestSuite):
 
             self.a1_r1.fcu.DetachVpnGateway(VpcId=self.vpc_info[VPC_ID], VpnGatewayId=self.vgw_id)
             wait_tools.wait_vpn_gateways_attachment_state(self.a1_r1, [self.vgw_id], 'detached')
+
 
     def test_T1948_test_vpn_static(self):
         self.exec_test_vpn(static=False, racoon=True, default_rtb=True)
