9e0f57cf27fd7c60fe572828a3d712eeec1e8471
lars.smit <lars.smit@outscale.com>
2021-03-04 13:02:14 +0100

    cosmetic

--- qa_tina_tests/USER/API/FCU/Snapshots/test_CreateSnapshotExportTask.py
+++ qa_tina_tests/USER/API/FCU/Snapshots/test_CreateSnapshotExportTask.py
@@ -237,12 +237,12 @@ class Test_CreateSnapshotExportTask(OscTestSuite):
     def test_T3896_with_invalid_ak_sk(self):
         ret = self.a1_r1.fcu.CreateSnapshotExportTask(SnapshotId=self.snap_id, ExportToOsu={'DiskImageFormat': 'qcow2',
                                                                                             'OsuBucket': self.bucket_name,
-                                                                                            'aksk':{'AccessKey': 'foo', 'SecretKey': 'bar'}})
+                                                                                           'aksk':{'AccessKey': 'foo', 'SecretKey': 'bar'}})
         task_id = ret.response.snapshotExportTask.snapshotExportTaskId
         try:
             wait_snapshot_export_tasks_state(osc_sdk=self.a1_r1, state='failed', snapshot_export_task_id_list=[task_id])
             assert False, 'Remove known error'
-        except AssertionError as error:
+        except AssertionError:
             known_error('TINA-6147', 'Create export snapshot task with invalid ak/sk should have the failed state')
             raise error
         ret = self.a1_r1.fcu.DescribeSnapshotExportTasks(SnapshotExportTaskId=[task_id])
