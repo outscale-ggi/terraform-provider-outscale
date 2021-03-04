e036f463f35ce6315a0ac8df2490a4e33f0e7691
taher.kriaa <taher.kriaa@outscale.com>
2021-03-04 10:46:41 +0100

    Update

--- qa_tina_tests/USER/API/OAPI/RouteTable/test_ReadRouteTables.py
+++ qa_tina_tests/USER/API/OAPI/RouteTable/test_ReadRouteTables.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 import pytest
 
-from qa_test_tools.test_base import OscTestSuite, known_error
+from qa_test_tools.test_base import OscTestSuite
 from qa_tina_tools.tools.tina.create_tools import create_vpc
 from qa_tina_tools.tools.tina.delete_tools import delete_vpc
 from qa_tina_tools.tools.tina.info_keys import ROUTE_TABLE_ID, INTERNET_GATEWAY_ID, VPC_ID, SUBNETS, SUBNET_ID
