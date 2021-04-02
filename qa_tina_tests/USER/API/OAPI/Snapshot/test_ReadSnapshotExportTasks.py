d36278ee70367308d1054a4e565f4d9704aa21d6
ghassen.gabsi <ghassen.gabsi@outscale.com>
2021-04-02 09:21:24 +0200

    update

--- qa_tina_tests/USER/API/OAPI/Snapshot/test_ReadSnapshotExportTasks.py
+++ qa_tina_tests/USER/API/OAPI/Snapshot/test_ReadSnapshotExportTasks.py
@@ -1,12 +1,11 @@
 import pytest
 
 from qa_test_tools.misc import assert_dry_run
-from qa_test_tools.test_base import OscTestSuite
+from qa_tina_tests.USER.API.OAPI.Snapshot.Snapshot import Snapshot
 
 
-@pytest.mark.region_synchro_osu
-@pytest.mark.region_osu
-class Test_ReadSnapshotExportTasks(OscTestSuite):
+@pytest.mark.region_storageservice
+class Test_ReadSnapshotExportTasks(Snapshot):
 
     @classmethod
     def setup_class(cls):
@@ -17,8 +16,20 @@ class Test_ReadSnapshotExportTasks(OscTestSuite):
         super(Test_ReadSnapshotExportTasks, cls).teardown_class()
 
     def test_T3005_empty_filters(self):
-        ret = self.a1_r1.oapi.ReadSnapshotExportTasks()
-        ret.check_response()
+        snapshot_id = self.a1_r1.oapi.CreateSnapshot(VolumeId=self.volume_id1).response.Snapshot.SnapshotId
+        try:
+            self.a1_r1.oapi.CreateSnapshotExportTask(SnapshotId=snapshot_id,
+                                                     OsuExport={'DiskImageFormat': 'qcow2',
+                                                                'OsuBucket': 'snap-569',
+                                                                'OsuPrefix': '/foo%bar&'})
+            ret = self.a1_r1.oapi.ReadSnapshotExportTasks()
+            for task in ret.response.SnapshotExportTasks:
+                if task.SnapshotId == snapshot_id:
+                    assert task.OsuExport.OsuPrefix == '/foo%bar&'
+            ret.check_response()
+        finally:
+            if snapshot_id:
+                self.a1_r1.oapi.DeleteSnapshot(SnapshotId=snapshot_id)
 
     def test_T3006_valid_param_dry_run(self):
         ret = self.a1_r1.oapi.ReadSnapshotExportTasks(DryRun=True)
