40f63fdbe1b9b5ee5ea9c49225285fb7bbcadfeb
lars.smit <lars.smit@outscale.com>
2021-04-03 21:54:16 +0200

    changed error codes

--- qa_tina_tests/USER/API/OAPI/ClientGateway/test_CreateClientGateway.py
+++ qa_tina_tests/USER/API/OAPI/ClientGateway/test_CreateClientGateway.py
@@ -53,7 +53,7 @@ class Test_CreateClientGateway(OscTestSuite):
             self.a1_r1.oapi.CreateClientGateway(BgpAsn=10, PublicIp='172.10.8.9', ConnectionType='tata')
             assert False, 'Call should not have been successful'
         except OscApiException as error:
-            assert_oapi_error(error, 400, 'InvalidParameterValue', '4047')
+            assert_oapi_error(error, 400, 'InvalidParameterValue', '4129')
 
     def test_T3312_invalid_bgp_asn(self):
         try:
@@ -72,7 +72,7 @@ class Test_CreateClientGateway(OscTestSuite):
             self.a1_r1.oapi.CreateClientGateway(BgpAsn=0, PublicIp='tata', ConnectionType='ipsec.1')
             assert False, 'Call should not have been successful'
         except OscApiException as error:
-            assert_oapi_error(error, 400, 'InvalidParameterValue', '4047')
+            assert_oapi_error(error, 400, 'InvalidParameterValue', '4133')
         try:
             self.a1_r1.oapi.CreateClientGateway(BgpAsn=0, PublicIp='2001:0db8:0000:85a3:0000:0000:ac1f:8001', ConnectionType='ipsec.1')
             assert False, 'Call should not have been successful'
