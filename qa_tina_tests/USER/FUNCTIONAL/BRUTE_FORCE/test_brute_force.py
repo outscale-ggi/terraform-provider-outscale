2c6b4070317dccc4795f8166002dd763a29f6e0e
ghassen.gabsi <ghassen.gabsi@outscale.com>
2021-04-20 01:06:16 +0200

    update

---
+++ qa_tina_tests/USER/FUNCTIONAL/BRUTE_FORCE/test_brute_force.py
@@ -0,0 +1,118 @@
+import time
+
+
+import pytest
+
+
+from qa_sdk_pub import osc_api
+from qa_sdks import OscSdk
+
+from qa_test_tools.config import config_constants as constants, OscConfig
+from qa_test_tools.misc import assert_error, assert_oapi_error
+from qa_test_tools.test_base import OscTestSuite
+from qa_sdk_common.exceptions.osc_exceptions import OscApiException
+
+
+@pytest.mark.region_admin
+class Test_brute_force(OscTestSuite):
+
+    @classmethod
+    def setup_class(cls):
+        super(Test_brute_force, cls).setup_class()
+        cls.a1_r1.identauth.IdauthAccount.putAccountBruteForceProtectionPolicy(
+            bruteForceProtectionPolicy={"attemptThreshold": 5})
+        cls.a1_r1.identauth__admin.IdauthAdmin.invalidateCache(
+            account_id=cls.a1_r1.config.region.get_info(constants.AS_IDAUTH_ID))
+
+    @classmethod
+    def teardown_class(cls):
+        super(Test_brute_force, cls).teardown_class()
+
+    def test_T5644_ak_sk(self):
+        access_key = None
+        ret_create = self.a1_r1.oapi.CreateAccessKey()
+        access_key = ret_create.response.AccessKey.AccessKeyId
+        secret_key = ret_create.response.AccessKey.SecretKey
+        account_sdk = OscSdk(config=OscConfig.get_with_keys(
+            az_name=self.a1_r1.config.region.az_name,
+            ak=access_key,
+            sk=secret_key))
+        ret = account_sdk.lbu.DescribeLoadBalancers(exec_data={osc_api.EXEC_DATA_MAX_RETRY: 0})
+        wrong_account_sdk = OscSdk(config=OscConfig.get_with_keys(
+            az_name=self.a1_r1.config.region.az_name,
+            ak=access_key,
+            sk=secret_key + "AB"))
+        try:
+
+            for i in range(6):
+                try:
+                    ret = wrong_account_sdk.oapi.ReadVms(exec_data={osc_api.EXEC_DATA_MAX_RETRY: 0})
+                    time.sleep(2)
+                    print(ret)
+                except OscApiException as error:
+                    assert_oapi_error(error, 401, 'AccessDenied', 1)
+                    print(i)
+                    print(error)
+            try:
+                time.sleep(2)
+                wrong_account_sdk.oapi.ReadVms(exec_data={osc_api.EXEC_DATA_MAX_RETRY: 0})
+            except OscApiException as error:
+                assert_oapi_error(error, 401, 'AccessDenied', 1)
+            try:
+                time.sleep(2)
+                account_sdk.oapi.ReadVms(exec_data={osc_api.EXEC_DATA_MAX_RETRY: 0})
+            except OscApiException as error:
+                assert_oapi_error(error, 401, 'AccessDenied', 1)
+            time.sleep(2)
+            self.a1_r1.oapi.ReadVms(exec_data={osc_api.EXEC_DATA_MAX_RETRY: 0})
+            dict_services = {'eim': 'ListUsers', 'directlink': 'DescribeLocations', 'fcu': 'DescribeVolumes',
+                             'icu': 'ListAccessKeys', 'lbu': 'DescribeLoadBalancers'}
+            for service, call in dict_services.items():
+                try:
+                    eval('account_sdk' + '.' + service + '.' + call)(exec_data={osc_api.EXEC_DATA_MAX_RETRY: 0})
+                except OscApiException as error:
+                    assert_error(error, 401, 'AuthFailure', 'Outscale was not able to validate the provided access '
+                                                            'credentials. Invalid login/password or password has expired.')
+        except Exception as error:
+            raise error
+        finally:
+            if access_key:
+                self.a1_r1.oapi.DeleteAccessKey(AccessKeyId=access_key)
+
+    def test_T5645_login_password(self):
+        for i in range(6):
+            try:
+                ret = self.a1_r1.icu.AuthenticateAccount(
+                    exec_data={osc_api.EXEC_DATA_AUTHENTICATION: osc_api.AuthMethod.Empty,
+                               osc_api.EXEC_DATA_MAX_RETRY: 0},
+                    Login=self.a1_r1.config.account.login, Password=self.a1_r1.config.account.password+'h')
+                time.sleep(2)
+                print(ret)
+            except OscApiException as error:
+                assert_error(error, 401, 'AuthFailure', 'Outscale was not able to validate the provided access '
+                                                        'credentials. Invalid login/password or password has expired.')
+                print(i)
+                print(error)
+        try:
+            time.sleep(2)
+            ret = self.a1_r1.icu.AuthenticateAccount(
+                exec_data={osc_api.EXEC_DATA_AUTHENTICATION: osc_api.AuthMethod.Empty,
+                           osc_api.EXEC_DATA_MAX_RETRY: 0},
+                Login=self.a1_r1.config.account.login, Password=self.a1_r1.config.account.password + 'h')
+        except OscApiException as error:
+            assert_error(error, 500, 'IcuServerException', 'Internal Error')
+            print(error)
+        try:
+            time.sleep(2)
+            ret = self.a1_r1.icu.AuthenticateAccount(
+                exec_data={osc_api.EXEC_DATA_AUTHENTICATION: osc_api.AuthMethod.Empty,
+                           osc_api.EXEC_DATA_MAX_RETRY: 0},
+                Login=self.a1_r1.config.account.login, Password=self.a1_r1.config.account.password + 'h')
+        except OscApiException as error:
+            print(error)
+            assert_error(error, 500, 'IcuServerException', 'Internal Error')
+        try:
+            self.a1_r1.oapi.CheckAuthentication(Login=self.a1_r1.config.account.login,
+                                                Password=self.a1_r1.config.account.password)
+        except OscApiException as error:
+            assert_oapi_error(error, 500, 'InternalError', 2000)
--- qa_tina_tests/USER/FUNCTIONAL/OAPI/NetAccessPoint/test_net_access_point.py
+++ qa_tina_tests/USER/FUNCTIONAL/OAPI/NetAccessPoint/test_net_access_point.py
@@ -1,8 +1,8 @@
 import pytest
 
-from qa_common_tools.ssh import SshTools
+from qa_common_tools.ssh import SshTools, OscSshError
 from qa_test_tools.exceptions.test_exceptions import OscTestException
-from qa_test_tools.test_base import OscTestSuite
+from qa_test_tools.test_base import OscTestSuite, known_error
 from qa_test_tools.config import config_constants
 from qa_tina_tools.tina import oapi, info_keys, wait
 
@@ -43,9 +43,9 @@ class Test_net_access_point(OscTestSuite):
                 tmp_list.reverse()
                 net_access_point_service_name = '{}.{}'.format('.'.join(tmp_list), self.a1_r1.config.region.get_info(config_constants.STORAGESERVICE))
             net_access_point = self.a1_r1.oapi.CreateNetAccessPoint(
-                    NetId=net_with_internet_info[info_keys.NET_ID],
-                    ServiceName=net_access_point_service_name,
-                    RouteTableIds=[net_with_internet_info[info_keys.SUBNETS][2][info_keys.ROUTE_TABLE_ID]]).response.NetAccessPoint
+                NetId=net_with_internet_info[info_keys.NET_ID],
+                ServiceName=net_access_point_service_name,
+                RouteTableIds=[net_with_internet_info[info_keys.SUBNETS][2][info_keys.ROUTE_TABLE_ID]]).response.NetAccessPoint
 
             wait.wait_Vms_state(self.a1_r1, [net_with_internet_info[info_keys.SUBNETS][0][info_keys.VM_IDS][0]],
                                 state='ready')
@@ -69,6 +69,8 @@ class Test_net_access_point(OscTestSuite):
                 retry=4, timeout=10)
             out, _, _ = SshTools.exec_command_paramiko(sshclient_jhost, cmd, retry=20, timeout=20)
             assert 'Access Denied' in out
+        except OscSshError:
+            known_error('OPS-12734', "Cannot request a service after create a netaccespoint for it in SEC1")
         finally:
             errors = []
             if net_access_point:
@@ -83,3 +85,70 @@ class Test_net_access_point(OscTestSuite):
                     errors.append(error)
             if errors:
                 raise OscTestException('Found {} errors while cleaning resources : \n{}'.format(len(errors), [str(err) for err in errors]))
+
+    @pytest.mark.region_storageservice
+    def test_T5646_netaccesspoint_for_api_service(self):
+        net_with_internet_info = None
+        net_access_point = None
+        net_access_point_service_name = None
+        try:
+            net_with_internet_info = oapi.create_Net(self.a1_r1, nb_subnet=3, nb_vm=1, state=None,
+                                                     cidr_prefix=PUBLIC_NET_IP_RANGE_SUFFIX)
+            self.a1_r1.oapi.CreateSecurityGroupRule(
+                SecurityGroupId=net_with_internet_info[info_keys.SUBNETS][1][info_keys.SECURITY_GROUP_ID],
+                IpProtocol='icmp', FromPortRange=-1, ToPortRange=-1, Flow='Inbound',
+                IpRange='0.0.0.0/0')
+            for index in [1, 2]:
+                self.a1_r1.oapi.CreateSecurityGroupRule(
+                    SecurityGroupId=net_with_internet_info[info_keys.SUBNETS][index][info_keys.SECURITY_GROUP_ID],
+                    IpProtocol='tcp', FromPortRange=22, ToPortRange=22, Flow='Inbound',
+                    IpRange=net_with_internet_info[info_keys.SUBNETS][0][info_keys.IP_RANGE])
+            tmp_list = self.a1_r1.config.region.get_info(config_constants.HOST).split('.')
+            tmp_list.reverse()
+            net_access_point_service_name = '{}.api'.format('.'.join(tmp_list))
+            net_access_point = self.a1_r1.oapi.CreateNetAccessPoint(
+                NetId=net_with_internet_info[info_keys.NET_ID],
+                ServiceName=net_access_point_service_name,
+                RouteTableIds=[
+                    net_with_internet_info[info_keys.SUBNETS][2][info_keys.ROUTE_TABLE_ID]]).response.NetAccessPoint
+
+            wait.wait_Vms_state(self.a1_r1, [net_with_internet_info[info_keys.SUBNETS][0][info_keys.VM_IDS][0]],
+                                state='ready')
+
+            sshclient = SshTools.check_connection_paramiko(
+                net_with_internet_info[info_keys.SUBNETS][0][info_keys.PUBLIC_IP]['PublicIp'],
+                net_with_internet_info[info_keys.KEY_PAIR][info_keys.PATH],
+                username=self.a1_r1.config.region.get_info(config_constants.CENTOS_USER), retry=4, timeout=10)
+            tmp_list = net_access_point_service_name.split('.')
+            tmp_list.reverse()
+            cmd = "curl -X POST https://{}".format('.'.join(tmp_list))
+            cmd += "/api/latest"
+            wait.wait_Vms_state(self.a1_r1, [net_with_internet_info[info_keys.SUBNETS][2][info_keys.VM_IDS][0]],
+                                state='ready')
+            sshclient_jhost = SshTools.check_connection_paramiko_nested(
+                sshclient=sshclient,
+                ip_address=net_with_internet_info[info_keys.SUBNETS][0][info_keys.PUBLIC_IP]['PublicIp'],
+                ssh_key=net_with_internet_info[info_keys.KEY_PAIR][info_keys.PATH],
+                local_private_addr=net_with_internet_info[info_keys.SUBNETS][0][info_keys.VMS][0]['PrivateIp'],
+                dest_private_addr=net_with_internet_info[info_keys.SUBNETS][2][info_keys.VMS][0]['PrivateIp'],
+                username=self.a1_r1.config.region.get_info(config_constants.CENTOS_USER),
+                retry=4, timeout=10)
+            out, _, _ = SshTools.exec_command_paramiko(sshclient_jhost, cmd, retry=20, timeout=20)
+            assert 'Version' in out
+        except OscSshError:
+            known_error('OPS-12734', "Cannot request a service after create a netaccespoint for it in SEC1")
+        finally:
+            errors = []
+            if net_access_point:
+                try:
+                    self.a1_r1.oapi.DeleteNetAccessPoint(NetAccessPointId=net_access_point.NetAccessPointId)
+                except Exception as error:
+                    errors.append(error)
+            if net_with_internet_info:
+                try:
+                    oapi.delete_Net(self.a1_r1, net_with_internet_info)
+                except Exception as error:
+                    errors.append(error)
+            if errors:
+                raise OscTestException(
+                    'Found {} errors while cleaning resources : \n{}'.format(len(errors), [str(err) for err in errors]))
