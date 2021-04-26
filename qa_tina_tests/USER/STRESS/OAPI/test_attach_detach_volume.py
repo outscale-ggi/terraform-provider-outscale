892416778ec9d8b77a95c6b1b9b6b8b331441159
akram.arab <akram.arab@outscale.com>
2021-04-26 15:39:20 +0200

    Add test PQA-2774 Check multiple snap and multiple attach detach to avoid bad descriptor error

---
+++ qa_tina_tests/USER/STRESS/OAPI/test_attach_detach_volume.py
@@ -0,0 +1,259 @@
+import time
+import pytest
+from qa_test_tools.test_base import OscTestSuite, known_error
+from qa_test_tools.config import config_constants
+from qa_common_tools.ssh import SshTools
+from qa_tina_tools.tina import wait, oapi, info_keys
+from qa_tina_tools.tina.check_tools import format_mount_volume, umount_volume
+from qa_tina_tests.USER.API.OAPI.Snapshot.Snapshot import validate_snasphot
+
+DEVICE_NAME = '/dev/xvdc'
+CMD = 'ls -lsa /dev/x*'
+
+# NB_ITER is the number of iteration in this test.
+# The assigned value is requested in PQA-2774.
+NB_ITER = 20
+
+class Test_attach_detach_volume(OscTestSuite):
+
+    @classmethod
+    def setup_class(cls):
+        super(Test_attach_detach_volume, cls).setup_class()
+        cls.vm_info = None
+        cls.sshclient = None
+        cls.vol_id = None
+        cls.ret_link = None
+        cls.volume_mount = None
+        cls.is_attached = False
+        cls.snap_ids = None
+
+    @classmethod
+    def teardown_class(cls):
+        super(Test_attach_detach_volume, cls).teardown_class()
+
+    def setup_method(self, method):
+        super(Test_attach_detach_volume, self).setup_method(method)
+        self.volume_mount = '/mnt/vol'
+        self.snap_ids = []
+        try:
+            self.vm_info = oapi.create_Vms(self.a1_r1, state='ready')
+
+            self.vol_id = self.a1_r1.oapi.CreateVolume(VolumeType='standard', Size=2,
+                                                        SubregionName=self.azs[0]).response.Volume.VolumeId
+
+            wait.wait_Volumes_state(self.a1_r1, [self.vol_id], state='available')
+
+            self.ret_link = self.a1_r1.oapi.LinkVolume(VolumeId=self.vol_id,
+                                                        VmId=self.vm_info[info_keys.VM_IDS][0],
+                                                        DeviceName=DEVICE_NAME)
+            self.is_attached = True
+
+            wait.wait_Volumes_state(self.a1_r1, [self.vol_id], state='in-use')            
+
+            self.sshclient = SshTools.check_connection_paramiko(self.vm_info[info_keys.VMS][0]['PublicIp'],
+                                                                self.vm_info[info_keys.KEY_PAIR][info_keys.PATH],
+                                                                username=self.a1_r1.config.region.get_info(
+                                                                    config_constants.CENTOS_USER))
+
+            format_mount_volume(self.sshclient, DEVICE_NAME, self.volume_mount, True)
+            time.sleep(2)
+            umount_volume(self.sshclient, self.volume_mount)
+
+            self.a1_r1.oapi.UnlinkVolume(VolumeId=self.vol_id)
+            self.is_attached = False
+            wait.wait_Volumes_state(self.a1_r1, [self.vol_id], state='available')
+        except:
+            try:
+                self.teardown_method(method)
+            finally:
+                raise
+
+    def teardown_method(self, method):
+        try:
+            if self.is_attached:
+                self.a1_r1.oapi.UnlinkVolume(VolumeId=self.vol_id)
+                wait.wait_Volumes_state(self.a1_r1, [self.vol_id], state='available')
+            if self.snap_ids:
+                for snap_id in self.snap_ids:
+                    self.a1_r1.oapi.DeleteSnapshot(SnapshotId=snap_id)
+                wait.wait_Snapshots_state(self.a1_r1, self.snap_ids, cleanup=True)
+            if self.vol_id:
+                self.a1_r1.oapi.DeleteVolume(VolumeId=self.vol_id)
+            if self.vm_info:
+                oapi.delete_Vms(self.a1_r1, self.vm_info)
+        except Exception as error:
+            self.logger.exception(error)
+            pytest.fail("An unexpected error happened : " + str(error))
+        finally:
+            super(Test_attach_detach_volume, self).teardown_method(method)
+
+    def test_T0000_multi_snap_and_attach_detach(self):
+        """
+            This test have two goal.
+            The first loop aim to create snapshot
+            The second loop aim to attach a volume and mount it,
+            then umount and detach volume.
+        """
+        for _ in range(NB_ITER):
+            try:
+                ret = self.a1_r1.oapi.CreateSnapshot(VolumeId=self.vol_id, Description='hello').response.Snapshot
+                self.snap_ids.append(ret.SnapshotId)
+
+                wait.wait_Snapshots_state(self.a1_r1, self.snap_ids, state='completed')
+
+                validate_snasphot(ret, expected_snap={
+                    'Description': 'hello',
+                    'AccountId': self.a1_r1.config.account.account_id,
+                    'VolumeSize': 2,
+                })
+                assert ret.State is not None
+            except Exception as error:
+                self.logger.exception(error)
+                pytest.fail("An unexpected error happened : " + str(error))
+
+        for _ in range(NB_ITER):
+            try:
+                self.ret_link = self.a1_r1.oapi.LinkVolume(VolumeId=self.vol_id,
+                                                            VmId=self.vm_info[info_keys.VM_IDS][0],
+                                                            DeviceName=DEVICE_NAME)
+                self.is_attached = True
+
+                try:
+                    wait.wait_Volumes_state(self.a1_r1, [self.vol_id], state='in-use')
+                except:
+                    ret = self.a1_r1.oapi.ReadVolumes(Filters={"VolumeIds": [self.vol_id]})
+                    if ret.response.Volumes[0].state == "attaching":
+                        known_error ("TINA-6448", "Volume remains attaching")
+                    raise
+
+                out, status, _ = SshTools.exec_command_paramiko(self.sshclient, CMD)
+                assert status == 0
+                assert DEVICE_NAME in out
+
+                cmd_mount = 'sudo mount -o nouuid {} {}'.format(DEVICE_NAME, self.volume_mount)
+                SshTools.exec_command_paramiko(self.sshclient, cmd_mount)
+
+                time.sleep(2)
+
+                umount_volume(self.sshclient, self.volume_mount)
+            except Exception as error:
+                self.logger.exception(error)
+                pytest.fail("An unexpected error happened : " + str(error))
+            finally:
+                if self.ret_link:
+                    self.a1_r1.oapi.UnlinkVolume(VolumeId=self.vol_id)
+                    self.is_attached = False
+                    wait.wait_Volumes_state(self.a1_r1, [self.vol_id], state='available')
+
+                    _, status, _ = SshTools.exec_command_paramiko(self.sshclient, CMD, expected_status=2)
+                    assert status != 0
+
+    def test_T0000_create_snap_from_attached_volume(self):
+        """
+            This test aim to attach a volume and mount it,
+            then create a snapshot from attached volume.
+        """
+        try:
+            self.ret_link = self.a1_r1.oapi.LinkVolume(VolumeId=self.vol_id,
+                                                        VmId=self.vm_info[info_keys.VM_IDS][0],
+                                                        DeviceName=DEVICE_NAME)
+            self.is_attached = True
+
+            wait.wait_Volumes_state(self.a1_r1, [self.vol_id], state='in-use')
+
+            out, status, _ = SshTools.exec_command_paramiko(self.sshclient, CMD)
+            assert status == 0
+            assert DEVICE_NAME in out
+
+            cmd_mount = 'sudo mount -o nouuid {} {}'.format(DEVICE_NAME, self.volume_mount)
+            SshTools.exec_command_paramiko(self.sshclient, cmd_mount)
+            time.sleep(2)
+
+            for _ in range(NB_ITER):
+                try:
+                    ret = self.a1_r1.oapi.CreateSnapshot(VolumeId=self.vol_id, Description='hello').response.Snapshot
+                    self.snap_ids.append(ret.SnapshotId)
+
+                    try:
+                        wait.wait_Snapshots_state(self.a1_r1, self.snap_ids, state='completed')
+                    except:
+                        ret = self.a1_r1.oapi.ReadSnapshots(Filters={"SnapshotIds": [ret.SnapshotId]})
+                        if ret.response.Snapshots[0].State == "pending":
+                            known_error ("OPS-13374", "Bad file descriptor")
+                        raise
+
+                    validate_snasphot(ret, expected_snap={
+                        'Description': 'hello',
+                        'AccountId': self.a1_r1.config.account.account_id,
+                        'VolumeSize': 2,
+                    })
+                    assert ret.State is not None
+                except Exception as error:
+                    self.logger.exception(error)
+                    pytest.fail("An unexpected error happened : " + str(error))
+
+            umount_volume(self.sshclient, self.volume_mount)
+        except Exception as error:
+            self.logger.exception(error)
+            pytest.fail("An unexpected error happened : " + str(error))
+        finally:
+            if self.ret_link:
+                self.a1_r1.oapi.UnlinkVolume(VolumeId=self.vol_id)
+                self.is_attached = False
+                wait.wait_Volumes_state(self.a1_r1, [self.vol_id], state='available')
+
+                _, status, _ = SshTools.exec_command_paramiko(self.sshclient, CMD, expected_status=2)
+                assert status != 0
+
+    def test_T0000_attach_detach_vol_and_create_snap(self):
+        """
+            This test aim to attach a volume and mount it,
+            write text file and umount and detach volume,
+            then create a snapshot for volume.
+        """
+        for _ in range(NB_ITER):
+            try:
+                self.ret_link = self.a1_r1.oapi.LinkVolume(VolumeId=self.vol_id,
+                                                            VmId=self.vm_info[info_keys.VM_IDS][0],
+                                                            DeviceName=DEVICE_NAME)
+                self.is_attached = True
+
+                wait.wait_Volumes_state(self.a1_r1, [self.vol_id], state='in-use')
+
+                out, status, _ = SshTools.exec_command_paramiko(self.sshclient, CMD)
+                assert status == 0
+                assert DEVICE_NAME in out
+
+                cmd_mount = 'sudo mount -o nouuid {} {}'.format(DEVICE_NAME, self.volume_mount)
+                SshTools.exec_command_paramiko(self.sshclient, cmd_mount)
+
+                cmd_write_data = 'sudo openssl rand -out /mnt/vol/data_xxx.txt -base64 $(({} * 2**20 * 3/4))'.format(100)
+                SshTools.exec_command_paramiko(self.sshclient, cmd_write_data, eof_time_out=300)
+
+                umount_volume(self.sshclient, self.volume_mount)
+                if self.ret_link:
+                    self.a1_r1.oapi.UnlinkVolume(VolumeId=self.vol_id)
+                    wait.wait_Volumes_state(self.a1_r1, [self.vol_id], state='available')
+                    self.is_attached = False
+
+                    _, status, _ = SshTools.exec_command_paramiko(self.sshclient, CMD, expected_status=2)
+                    assert status != 0
+
+                    try:
+                        ret = self.a1_r1.oapi.CreateSnapshot(VolumeId=self.vol_id, Description='hello').response.Snapshot
+                        self.snap_ids.append(ret.SnapshotId)
+
+                        wait.wait_Snapshots_state(self.a1_r1, self.snap_ids, state='completed')
+
+                        validate_snasphot(ret, expected_snap={
+                            'Description': 'hello',
+                            'AccountId': self.a1_r1.config.account.account_id,
+                            'VolumeSize': 2,
+                        })
+                        assert ret.State is not None
+                    except Exception as error:
+                        self.logger.exception(error)
+                        pytest.fail("An unexpected error happened : " + str(error))
+            except Exception as error:
+                self.logger.exception(error)
+                pytest.fail("An unexpected error happened : " + str(error))
