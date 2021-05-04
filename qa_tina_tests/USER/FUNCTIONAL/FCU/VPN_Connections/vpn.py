6e15589c2f0639bfa0e91fab815cdc57074d3e45
taher.kriaa <taher.kriaa@outscale.com>
2021-05-04 13:42:52 +0200

    fix vpn tests

--- qa_tina_tests/USER/FUNCTIONAL/FCU/VPN_Connections/vpn.py
+++ qa_tina_tests/USER/FUNCTIONAL/FCU/VPN_Connections/vpn.py
@@ -20,8 +20,8 @@ from qa_tina_tools.tools.tina.wait_tools import wait_vpn_connections_state
 def upgrade_ike_to_v2(sshclient, leftid, rightid):
     cmd = """
         sudo sed -i  's/^            keyexchange=.*/            keyexchange=ikev2/g'  /etc/strongswan/ipsec.conf;
-        sudo sed -i '$a{}' /etc/strongswan/ipsec.conf;
-        sudo sed -i '$a{}' /etc/strongswan/ipsec.conf;
+        sudo sed -i '$a\{}' /etc/strongswan/ipsec.conf;
+        sudo sed -i '$a\{}' /etc/strongswan/ipsec.conf;
         sudo systemctl stop strongswan; sudo systemctl start strongswan;""".format(leftid, rightid)
     _, _, _ = SshTools.exec_command_paramiko(
     sshclient, cmd, retry=20, timeout=10, eof_time_out=60)
@@ -34,7 +34,7 @@ def update_cgw_config(option, sshclient):
     cmd = """
         sudo sed -i  's/^            ike=.*/            ike={1}/g'  /etc/strongswan/ipsec.conf ;
         sudo sed -i  's/^            esp=.*/            esp={1}/g'  /etc/strongswan/ipsec.conf;
-        sudo systemctl stop strongswan;""".format(option)
+        sudo systemctl stop strongswan;""".format(option,option)
     _, _, _ = SshTools.exec_command_paramiko(sshclient, cmd, retry=20, timeout=10, eof_time_out=60)
 
     out, _, _ = SshTools.exec_command_paramiko(
