402aedcf25789c9dc63154312490c1276aaa9250
lars.smit <lars.smit@outscale.com>
2021-05-06 18:54:15 +0200

    pylint

--- qa_tina_tests/USER/API/KMS/Customer_Master_Keys/test_CreateKey.py
+++ qa_tina_tests/USER/API/KMS/Customer_Master_Keys/test_CreateKey.py
@@ -2,7 +2,7 @@ import pytest
 
 from qa_sdk_common.exceptions.osc_exceptions import OscApiException
 from qa_test_tools.misc import id_generator, assert_error
-from qa_test_tools.test_base import OscTestSuite, known_error
+from qa_test_tools.test_base import OscTestSuite
 from qa_tina_tests.USER.API.KMS.kms import Kms
 
 
