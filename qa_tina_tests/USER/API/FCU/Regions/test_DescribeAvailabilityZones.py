f31e1958320414d2927b6659956fcac97aeab9b2
Ulman NGOMA <ulman.ngoma@MacBook-Pro-de-Ulman.local>
2021-05-10 16:13:56 +0200

    added new test

--- qa_tina_tests/USER/API/FCU/Regions/test_DescribeAvailabilityZones.py
+++ qa_tina_tests/USER/API/FCU/Regions/test_DescribeAvailabilityZones.py
@@ -1,4 +1,5 @@
 from qa_test_tools.test_base import OscTestSuite
+from specs.check_tools import check_response
 
 
 class Test_DescribeAvailabilityZones(OscTestSuite):
@@ -10,3 +11,7 @@ class Test_DescribeAvailabilityZones(OscTestSuite):
     @classmethod
     def teardown_class(cls):
         super(Test_DescribeAvailabilityZones, cls).teardown_class()
+
+    def test_T5664_no_param(self):
+        ret = self.a1_r1.fcu.DescribeAvailabilityZones()
+        ret.check_response()
