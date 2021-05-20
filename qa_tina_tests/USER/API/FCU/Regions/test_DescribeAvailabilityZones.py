8eec75d973aa43d6ccdb24f3dd62b632edefd2a4
Ulman NGOMA <ulman.ngoma@MacBook-Pro-de-Ulman.local>
2021-05-20 17:07:15 +0200

    add test create vms and volumes

--- qa_tina_tests/USER/API/FCU/Regions/test_DescribeAvailabilityZones.py
+++ qa_tina_tests/USER/API/FCU/Regions/test_DescribeAvailabilityZones.py
@@ -1,3 +1,5 @@
+from qa_sdk_common.exceptions import OscApiException
+from qa_test_tools import misc
 from qa_test_tools.test_base import OscTestSuite
 
 
@@ -14,3 +16,17 @@ class Test_DescribeAvailabilityZones(OscTestSuite):
     def test_T5664_no_param(self):
         ret = self.a1_r1.fcu.DescribeAvailabilityZones()
         ret.check_response()
+
+    def test_T5665_dry_run(self):
+        try:
+            self.a1_r1.fcu.DescribeAvailabilityZones(DryRun=True)
+            assert False, "Call should not have been successful"
+        except OscApiException as error:
+            misc.assert_error(error, 400, 'DryRunOperation', 'Request would have succeeded, but DryRun flag is set.')
+
+    def test_T5666_unknown_param(self):
+        try:
+            self.a1_r1.fcu.DescribeAvailabilityZones(Foo="bar")
+            assert False, "Call should not have been successful"
+        except OscApiException as error:
+            misc.assert_error(error, 400, 'InvalidParameterValue', 'Unexpected parameter Foo')
