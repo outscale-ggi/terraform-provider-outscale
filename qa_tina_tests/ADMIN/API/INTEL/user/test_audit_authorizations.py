b2d4c5834092c49d31e92a3599c9d9de1f1684a7
lars.smit <lars.smit@outscale.com>
2021-05-26 12:54:37 +0200

    added known error OPS-13703

--- qa_tina_tests/ADMIN/API/INTEL/user/test_audit_authorizations.py
+++ qa_tina_tests/ADMIN/API/INTEL/user/test_audit_authorizations.py
@@ -1,4 +1,4 @@
-from qa_test_tools.test_base import OscTestSuite
+from qa_test_tools.test_base import OscTestSuite, known_error
 
 
 class Test_audit_authorizations(OscTestSuite):
@@ -17,4 +17,7 @@ class Test_audit_authorizations(OscTestSuite):
         assert len(ret.response.result.allocate_everywhere) >= 1
         assert len(ret.response.result.use_dedicated_owner) >= 1
         assert len(ret.response.result.use_internal_instance_types) >= 1
+        if len(ret.response.result.attach_all_nics) == 0:
+            known_error('OPS-13703', 'ows.internal does not have attach_all_nics rights')
+        assert False, 'Remove known error code'
         assert len(ret.response.result.attach_all_nics) >= 1
