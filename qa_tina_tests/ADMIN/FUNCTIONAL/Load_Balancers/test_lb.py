3220064feb09adf20ef9a96cc3b0a275ccedb122
lars.smit <lars.smit@outscale.com>
2021-05-30 17:37:46 +0200

    better, adapt to in1

--- qa_tina_tests/ADMIN/FUNCTIONAL/Load_Balancers/test_lb.py
+++ qa_tina_tests/ADMIN/FUNCTIONAL/Load_Balancers/test_lb.py
@@ -2,6 +2,7 @@ import time
 
 from qa_test_tools.misc import id_generator
 from qa_test_tools.test_base import OscTestSuite
+from qa_test_tools.exceptions.test_exceptions import OscTestException
 from qa_tina_tools.tools.tina.delete_tools import delete_lbu
 
 
@@ -30,17 +31,29 @@ class Test_lb(OscTestSuite):
         eip_values = []
         ips = []
         for _ in range(3):
-            lb_name = id_generator(prefix='lbu-')
-            ret = self.a1_r1.oapi.CreateLoadBalancer(
-                Listeners=[{'BackendPort': 65535, 'LoadBalancerProtocol': 'HTTP', 'LoadBalancerPort': 80}],
-                LoadBalancerName=lb_name, SubregionNames=[self.a1_r1.config.region.az_name],
-            )
-            lb_names.append(lb_name)
-            time.sleep(20)
-            ret = self.a1_r1.intel.netimpl.firewall.find_firewalls(filters={'resource': ret.response.LoadBalancer.DnsName.split('.')[0]})
-            ret = self.a1_r1.intel.tag.find(resource=ret.response.result[0].vm, key='osc.fcu.eip.auto-attach')
-            eip_values.append(ret.response.result[0].value)
-            delete_lbu(self.a1_r1, lbu_name=lb_name)
+            ret_lb = None
+            try:
+                lb_name = id_generator(prefix='lbu-')
+                ret_lb = self.a1_r1.oapi.CreateLoadBalancer(
+                    Listeners=[{'BackendPort': 65535, 'LoadBalancerProtocol': 'HTTP', 'LoadBalancerPort': 80}],
+                    LoadBalancerName=lb_name, SubregionNames=[self.a1_r1.config.region.az_name],
+                )
+                lb_names.append(lb_name)
+                count = 0
+                while count < 5:
+                    time.sleep(20)
+                    ret = self.a1_r1.intel.netimpl.firewall.find_firewalls(filters={'resource': ret_lb.response.LoadBalancer.DnsName.split('.')[0]})
+                    if ret.response.result:
+                        break
+                    count += 1
+                if count == 5:
+                    raise OscTestException('Could not find lbu firewall')
+                ret = self.a1_r1.intel.tag.find(resource=ret.response.result[0].vm, key='osc.fcu.eip.auto-attach')
+                eip_values.append(ret.response.result[0].value)
+            finally:
+                if ret_lb:
+                    delete_lbu(self.a1_r1, lbu_name=lb_name)
+        time.sleep(60)
         ret = self.a1_r1.intel.address.describe(owner=self.a1_r1.intel.user.get_details(username='ows.elb@outscale.com').response.result.username)
         for i in ret.response.result.results:
             ips.append(i.ip)
