4d449e5750e2ae014c223f9f1e7642c2d0685f36
lars.smit <lars.smit@outscale.com>
2021-06-18 22:26:54 +0200

    pylint

--- qa_tina_tests/USER/API/OAPI/AccessKeys/test_DeleteAccessKey.py
+++ qa_tina_tests/USER/API/OAPI/AccessKeys/test_DeleteAccessKey.py
@@ -2,7 +2,7 @@ from time import sleep
 
 import pytest
 
-from qa_sdk_common.exceptions.osc_exceptions import OscApiException, OscSdkException
+from qa_sdk_common.exceptions.osc_exceptions import OscApiException
 from qa_sdk_pub import osc_api
 from qa_test_tools import misc
 from qa_test_tools.test_base import OscTestSuite, known_error
--- qa_tina_tests/USER/API/OAPI/AccessKeys/test_UpdateAccessKey.py
+++ qa_tina_tests/USER/API/OAPI/AccessKeys/test_UpdateAccessKey.py
@@ -1,6 +1,6 @@
 import pytest
 
-from qa_sdk_common.exceptions.osc_exceptions import OscApiException, OscSdkException
+from qa_sdk_common.exceptions.osc_exceptions import OscApiException
 from qa_sdk_pub import osc_api
 from qa_test_tools import misc
 from qa_test_tools.misc import assert_dry_run
