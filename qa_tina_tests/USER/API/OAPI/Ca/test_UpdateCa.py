25608b3a1bb752409c265acee5c8ad38405fcce2
lars.smit <lars.smit@outscale.com>
2021-06-19 16:37:40 +0200

    added known error

--- qa_tina_tests/USER/API/OAPI/Ca/test_UpdateCa.py
+++ qa_tina_tests/USER/API/OAPI/Ca/test_UpdateCa.py
@@ -3,8 +3,9 @@ import pytest
 from qa_sdk_common.exceptions.osc_exceptions import OscApiException
 from qa_sdk_pub import osc_api
 from qa_test_tools.misc import assert_oapi_error, assert_dry_run
-from qa_test_tools.test_base import OscTestSuite
+from qa_test_tools.test_base import OscTestSuite, known_error
 from qa_tina_tools.tools.tina.create_tools import create_certificate_setup
+from qa_test_tools import misc
 
 
 class Test_UpdateCa(OscTestSuite):
@@ -52,8 +53,13 @@ class Test_UpdateCa(OscTestSuite):
             assert_oapi_error(error, 400, 'InvalidParameterValue', '4122')
 
     def test_T5725_login_password(self):
-        ret = self.a1_r1.oapi.UpdateCa(
-            exec_data={osc_api.EXEC_DATA_AUTHENTICATION: osc_api.AuthMethod.LoginPassword},
-            CaId=self.ca_id, Description='test update')
-        ret.check_response()
-        assert ret.response.Ca.Description == 'test update'
+        try:
+            ret = self.a1_r1.oapi.UpdateCa(
+                exec_data={osc_api.EXEC_DATA_AUTHENTICATION: osc_api.AuthMethod.LoginPassword},
+                CaId=self.ca_id, Description='test update')
+            assert False, 'Remove known error code'
+            ret.check_response()
+            assert ret.response.Ca.Description == 'test update'
+        except OscApiException as error:
+            misc.assert_oapi_error(error, 400, 'InvalidParameter', '3001')
+            known_error('GTW-1961', 'Login Password Authentication does not function')
