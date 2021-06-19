09fbe4725b7b28994910a19690585983483784d6
lars.smit <lars.smit@outscale.com>
2021-06-19 13:03:13 +0200

    pylint

--- qa_tina_tests/USER/API/DirectLink/Virtual_Interfaces/test_CreatePrivateVirtualInterface.py
+++ qa_tina_tests/USER/API/DirectLink/Virtual_Interfaces/test_CreatePrivateVirtualInterface.py
@@ -3,7 +3,7 @@ import pytest
 
 from qa_sdk_common.exceptions.osc_exceptions import OscApiException
 from qa_test_tools.misc import assert_error, id_generator
-from qa_test_tools.test_base import OscTestSuite, known_error
+from qa_test_tools.test_base import OscTestSuite
 
 
 @pytest.mark.region_admin
