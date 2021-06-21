3ac8ddf815e46a6d9a9bebefa03fac41e78e23d0
lars.smit <lars.smit@outscale.com>
2021-06-21 14:25:02 +0200

    pylint

--- qa_tina_tests/USER/API/OAPI/Vm/test_ReadVmsState.py
+++ qa_tina_tests/USER/API/OAPI/Vm/test_ReadVmsState.py
@@ -1,5 +1,6 @@
 
 from datetime import datetime, timedelta
+import time
 
 import pytest
 import pytz
@@ -7,12 +8,10 @@ import pytz
 from qa_sdk_common.exceptions.osc_exceptions import OscApiException
 from qa_test_tools.misc import assert_oapi_error, assert_dry_run
 from qa_test_tools.test_base import OscTestSuite, known_error
+from qa_test_tools.exceptions.test_exceptions import OscTestException
 from qa_tina_tools.tools.tina import info_keys
 from qa_tina_tools.tools.tina.create_tools import create_instances
 from qa_tina_tools.tools.tina.delete_tools import delete_instances
-from itertools import takewhile
-import time
-from qa_test_tools.exceptions.test_exceptions import OscTestException
 
 
 class Test_ReadVmsState(OscTestSuite):
