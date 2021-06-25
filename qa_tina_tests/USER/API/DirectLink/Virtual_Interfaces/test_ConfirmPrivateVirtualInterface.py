2e160366f477c2a3bc9b7fa0f18cb125bd0eeab5
lars.smit <lars.smit@outscale.com>
2021-06-25 10:20:23 +0200

    corrected tests

--- qa_tina_tests/USER/API/DirectLink/Virtual_Interfaces/test_ConfirmPrivateVirtualInterface.py
+++ qa_tina_tests/USER/API/DirectLink/Virtual_Interfaces/test_ConfirmPrivateVirtualInterface.py
@@ -15,21 +15,23 @@ class Test_ConfirmPrivateVirtualInterface(OscTestSuite):
     @classmethod
     def setup_class(cls):
         cls.conn_id = None
-        cls.vgw_id = None
         cls.quotas = {'dl_connection_limit': 1, 'dl_interface_limit': 1}
         super(Test_ConfirmPrivateVirtualInterface, cls).setup_class()
         ret = cls.a1_r1.directlink.DescribeLocations()
-        ret = cls.a1_r1.directlink.CreateConnection(location=ret.response.locations[0].locationCode, bandwidth='1Gbps',
-                                                    connectionName=id_generator(prefix='dl_'))
-        cls.conn_id = ret.response.connectionId
+        cls.location_code = ret.response.locations[0].locationCode
 
-    @classmethod
-    def teardown_class(cls):
+    def setup_method(self, method):
+        self.conn_id = None
+        OscTestSuite.setup_method(self, method)
+        ret = self.a1_r1.directlink.CreateConnection(location=self.location_code, bandwidth='1Gbps', connectionName=id_generator(prefix='dl_'))
+        self.conn_id = ret.response.connectionId
+
+    def teardown_method(self, method):
         try:
-            if cls.conn_id:
-                cls.a1_r1.intel.dl.connection.delete(owner=cls.a1_r1.config.account.account_id, connection_id=cls.conn_id)
+            if self.conn_id:
+                self.a1_r1.intel.dl.connection.delete(owner=self.a1_r1.config.account.account_id, connection_id=self.conn_id)
         finally:
-            super(Test_ConfirmPrivateVirtualInterface, cls).teardown_class()
+            OscTestSuite.teardown_method(self, method)
 
     def test_T1915_required_param(self):
         allocation = {'asn': 11111, 'virtualInterfaceName': 'test', 'vlan': 2}
