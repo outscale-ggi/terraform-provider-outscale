7f91aed41ec499fb21fce69d895bc99ebc6ebfa1
lars.smit <lars.smit@outscale.com>
2021-06-27 22:22:31 +0200

    better

--- qa_tina_tests/USER/API/DirectLink/Virtual_Interfaces/test_DescribeVirtualInterfaces.py
+++ qa_tina_tests/USER/API/DirectLink/Virtual_Interfaces/test_DescribeVirtualInterfaces.py
@@ -2,8 +2,6 @@
 import time
 import pytest
 
-from qa_sdk_common.exceptions.osc_exceptions import OscApiException
-from qa_test_tools import misc
 from qa_test_tools.test_base import OscTestSuite
 
 
