53f8a8f9dcd5abdff49dace1ab5b1e54f8cb8225
lars.smit <lars.smit@outscale.com>
2021-07-07 15:40:37 +0200

    pylint

--- qa_tina_tests/USER/FUNCTIONAL/EIM/Policies/test_create_policy.py
+++ qa_tina_tests/USER/FUNCTIONAL/EIM/Policies/test_create_policy.py
@@ -4,7 +4,7 @@ from qa_sdk_common.exceptions.osc_exceptions import OscApiException
 from qa_sdks.osc_sdk import OscSdk
 from qa_test_tools import misc
 from qa_test_tools.config import OscConfig, config_constants
-from qa_test_tools.test_base import OscTestSuite, known_error
+from qa_test_tools.test_base import OscTestSuite
 from qa_test_tools.config.region import Feature
 
 
