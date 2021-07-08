5e8d9f2af02d00444b3f8a5d690746448f477358
lars.smit <lars.smit@outscale.com>
2021-07-08 17:54:04 +0200

    pylint

--- qa_tina_tests/USER/FUNCTIONAL/OAPI/Volume/test_UpdateVolume_bootdisk_cold.py
+++ qa_tina_tests/USER/FUNCTIONAL/OAPI/Volume/test_UpdateVolume_bootdisk_cold.py
@@ -1,5 +1,4 @@
 
-import pytest
 from qa_common_tools.ssh import SshTools
 from qa_test_tools.config import config_constants as constants
 from qa_test_tools.test_base import OscTestSuite, known_error
