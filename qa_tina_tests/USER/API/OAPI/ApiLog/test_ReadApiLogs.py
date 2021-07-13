df9eefbcd67f806913abdc029f1135fde7614c7f
taher.kriaa <taher.kriaa@outscale.com>
2021-07-13 15:04:31 +0200

    QA-3171 OMS: add logs isolation tests

--- qa_tina_tests/USER/API/OAPI/ApiLog/test_ReadApiLogs.py
+++ qa_tina_tests/USER/API/OAPI/ApiLog/test_ReadApiLogs.py
@@ -35,11 +35,12 @@ class Test_ReadApiLogs(OscTestSuite):
     def setup_class(cls):
         super(Test_ReadApiLogs, cls).setup_class()
         cls.request_id = None
+        cls.request_id_a2 = None
         ret = cls.a1_r1.oapi.ReadTags()
         cls.request_id= ret.response.ResponseContext.RequestId
         cls.a1_r1.oapi.ReadSubnets()
         if hasattr(cls, "a2_r1"):
-            cls.a2_r1.oapi.ReadTags()
+            cls.request_id_a2 = cls.a2_r1.oapi.ReadTags().response.ResponseContext.RequestId
             cls.a2_r1.oapi.ReadSubnets()
         cls.a1_r1.oapi.ReadNics()
         cls.a1_r1.oapi.ReadKeypairs()
@@ -129,6 +130,7 @@ class Test_ReadApiLogs(OscTestSuite):
         assert ret.response.Logs[0].QueryCallName in call
         assert ret.response.Logs[1].QueryCallName in call
         assert ret.response.Logs[2].QueryCallName in call
+        assert {self.a1_r1.config.account.account_id} == {call.AccountId for call in ret.response.Logs}
 
     def test_T3205_verify_fcu_call(self):
         self.a1_r1.fcugtw.DescribeInstances()
@@ -137,21 +139,25 @@ class Test_ReadApiLogs(OscTestSuite):
             ResultsPerPage=100, Filters={'QueryDateAfter': (datetime.utcnow() - timedelta(seconds=100)).strftime('%Y-%m-%dT%H:%M:%S.%fZ')}
         )
         assert 'DescribeInstances' in [call.QueryCallName for call in ret.response.Logs]
+        assert {self.a1_r1.config.account.account_id} == {call.AccountId for call in ret.response.Logs}
 
     def test_T3206_valid_filter_QueryCallNames(self):
         ret = self.a1_r1.oapi.ReadApiLogs(Filters={"QueryCallNames": ["ReadVms"]}, ResultsPerPage=2)
         assert len(ret.response.Logs) == 2
         assert "ReadVms" in {call.QueryCallName for call in ret.response.Logs}
+        assert {self.a1_r1.config.account.account_id} == {call.AccountId for call in ret.response.Logs}
 
     def test_T3207_valid_filter_QueryAccessKeys(self):
         ret = self.a1_r1.oapi.ReadApiLogs(Filters={"QueryAccessKeys": [self.a1_r1.config.account.ak]})
         assert len(ret.response.Logs) != 0
         assert {self.a1_r1.config.account.ak} == {call.QueryAccessKey for call in ret.response.Logs}
+        assert {self.a1_r1.config.account.account_id} == {call.AccountId for call in ret.response.Logs}
 
     def test_T3208_valid_filter_QueryApiNames(self):
         ret = self.a1_r1.oapi.ReadApiLogs(Filters={"QueryApiNames": ["oapi", "aws_direct_connect", "aws_ec2"]}, ResultsPerPage=1000)
         assert len(ret.response.Logs) != 0
         assert {"oapi", "aws_direct_connect", "aws_ec2"} == {call.QueryApiName for call in ret.response.Logs}
+        assert {self.a1_r1.config.account.account_id} == {call.AccountId for call in ret.response.Logs}
 
     def test_T3209_invalid_QueryApiNames_value(self):
         ret = self.a1_r1.oapi.ReadApiLogs(Filters={"QueryApiNames": ["fcu", "lbu", "directlink", "eim", "icu"]})
@@ -160,6 +166,7 @@ class Test_ReadApiLogs(OscTestSuite):
     def test_T3210_valid_filter_QueryIpAddresses(self):
         ret = self.a1_r1.oapi.ReadApiLogs(Filters={"QueryIpAddresses": [misc.get_nat_ips(self.a1_r1.config.region)[0].split('/')[0]]})
         assert len(ret.response.Logs) != 0
+        assert {self.a1_r1.config.account.account_id} == {call.AccountId for call in ret.response.Logs}
 
     def test_T3211_valid_filter_QueryUserAgents(self):
         ret = self.a1_r1.oapi.ReadApiLogs(Filters={"QueryUserAgents": ["UNKNOWN"]})
@@ -178,24 +185,32 @@ class Test_ReadApiLogs(OscTestSuite):
     def test_T3214_valid_filter_QueryDateBefore(self):
         ret = self.a1_r1.oapi.ReadApiLogs(Filters={'QueryDateBefore': (datetime.utcnow()).strftime('%Y-%m-%dT%H:%M:%S.%fZ')})
         assert len(ret.response.Logs) != 0
+        assert {self.a1_r1.config.account.account_id} == {call.AccountId for call in ret.response.Logs}
         ret = self.a1_r1.oapi.ReadApiLogs(Filters={'QueryDateBefore': (datetime.utcnow()).strftime('%Y-%m-%d')})
         assert len(ret.response.Logs) != 0
+        assert {self.a1_r1.config.account.account_id} == {call.AccountId for call in ret.response.Logs}
 
     def test_T3215_valid_filter_QueryDateAfter(self):
         ret = self.a1_r1.oapi.ReadApiLogs(Filters={'QueryDateAfter': (datetime.utcnow() - timedelta(seconds=50)).strftime('%Y-%m-%dT%H:%M:%S.%fZ')})
         assert len(ret.response.Logs) != 0
+        assert {self.a1_r1.config.account.account_id} == {call.AccountId for call in ret.response.Logs}
         ret = self.a1_r1.oapi.ReadApiLogs(Filters={'QueryDateAfter': (datetime.utcnow() - timedelta(seconds=50)).strftime('%Y-%m-%d')})
         assert len(ret.response.Logs) != 0
+        assert {self.a1_r1.config.account.account_id} == {call.AccountId for call in ret.response.Logs}
 
     def test_T3216_valid_filter_combination(self):
         ret = self.a1_r1.oapi.ReadApiLogs(
             ResultsPerPage=5, Filters={'QueryDateAfter': (datetime.utcnow() - timedelta(5)).strftime('%Y-%m-%dT%H:%M:%S.%fZ')}
         )
+        assert {self.a1_r1.config.account.account_id} == {call.AccountId for call in ret.response.Logs}
         assert len(ret.response.Logs) == 5
         ret = self.a1_r1.oapi.ReadApiLogs(ResultsPerPage=5, Filters={"QueryApiNames": ["oapi"], "QueryAccessKeys": [self.a1_r1.config.account.ak]})
+        assert {self.a1_r1.config.account.account_id} == {call.AccountId for call in ret.response.Logs}
         assert len(ret.response.Logs) == 5
+        assert {self.a1_r1.config.account.account_id} == {call.AccountId for call in ret.response.Logs}
         ret = self.a1_r1.oapi.ReadApiLogs(ResultsPerPage=5, Filters={"QueryApiNames": ["oapi"], "ResponseStatusCodes": [200]})
         assert len(ret.response.Logs) == 5
+        assert {self.a1_r1.config.account.account_id} == {call.AccountId for call in ret.response.Logs}
 
     def test_T3217_invalid_filter_QueryDateAfter(self):
         try:
@@ -251,6 +266,7 @@ class Test_ReadApiLogs(OscTestSuite):
             assert hasattr(ret.response.Logs[0], attr)
             assert hasattr(ret.response.Logs[0], "AccountId")
             assert hasattr(ret.response.Logs[0], "RequestId")
+            assert {self.a1_r1.config.account.account_id} == {call.AccountId for call in ret.response.Logs}
 
     def test_T3223_invalid_With_Value(self):
         try:
@@ -281,6 +297,7 @@ class Test_ReadApiLogs(OscTestSuite):
             Filters={'QueryDateAfter': (datetime.utcnow() - timedelta(days=5)).strftime('%Y-%m-%dT%H:%M:%S.%fZ')},
             ResultsPerPage=1,
         )
+        assert {self.a1_r1.config.account.account_id} == {call.AccountId for call in ret.response.Logs}
         assert not ret.response.ResponseContext.RequestId == ret.response.Logs[0].RequestId
         for attr in param:
             assert hasattr(ret.response.Logs[0], attr)
@@ -288,10 +305,11 @@ class Test_ReadApiLogs(OscTestSuite):
     def test_T3201_valid_ResultsPerPage_value(self):
         ret = self.a1_r1.oapi.ReadApiLogs(ResultsPerPage=100)
         assert len(ret.response.Logs) != 0
-        # ret.check_response()
+        assert {self.a1_r1.config.account.account_id} == {call.AccountId for call in ret.response.Logs}
 
     def test_T3202_valid_NextPageToken_value(self):
         ret = self.a1_r1.oapi.ReadApiLogs(ResultsPerPage=2)
+        assert {self.a1_r1.config.account.account_id} == {call.AccountId for call in ret.response.Logs}
         assert len(ret.response.Logs) <= 2
         ret1 = self.a1_r1.oapi.ReadApiLogs(NextPageToken=ret.response.NextPageToken, ResultsPerPage=2)
         assert len(ret.response.Logs) <= 2
@@ -299,11 +317,13 @@ class Test_ReadApiLogs(OscTestSuite):
         assert ret.response.Logs[0].RequestId != ret1.response.Logs[1].RequestId
         assert ret.response.Logs[1].RequestId != ret1.response.Logs[0].RequestId
         assert ret.response.Logs[1].RequestId != ret1.response.Logs[1].RequestId
+        assert {self.a1_r1.config.account.account_id} == {call.AccountId for call in ret1.response.Logs}
 
     def test_T5550_valid_filter_multiple_QueryCallNames(self):
         ret = self.a1_r1.oapi.ReadApiLogs(Filters={"QueryCallNames": ["ReadVms", "DescribeConnections"]})
         assert len(ret.response.Logs) != 0
         assert {"ReadVms", "DescribeConnections"} == {call.QueryCallName for call in ret.response.Logs}
+        assert {self.a1_r1.config.account.account_id} == {call.AccountId for call in ret.response.Logs}
 
     def test_T5551_valid_filter_multiple_QueryAccessKeys(self):
         ret = self.a1_r1.oapi.ReadApiLogs(Filters={"QueryAccessKeys": [self.a1_r1.config.account.ak, self.a2_r1.config.account.ak]})
@@ -319,6 +339,7 @@ class Test_ReadApiLogs(OscTestSuite):
         ret = self.a1_r1.oapi.ReadApiLogs(With={'RequestId': False}, ResultsPerPage=1)
         assert hasattr(ret.response.Logs[0], "AccountId")
         assert not hasattr(ret.response.Logs[0], "RequestId")
+        assert {self.a1_r1.config.account.account_id} == {call.AccountId for call in ret.response.Logs}
 
     def test_T5554_With_RequestId(self):
         ret = self.a1_r1.oapi.ReadApiLogs(With={'AccountId': False}, ResultsPerPage=1)
@@ -329,3 +350,14 @@ class Test_ReadApiLogs(OscTestSuite):
         ret = self.a1_r1.oapi.ReadApiLogs(Filters={"RequestIds": [self.request_id]})
         assert len(ret.response.Logs) == 1
         assert ret.response.Logs[0].QueryCallName == "ReadTags"
+        assert {self.a1_r1.config.account.ak} == {call.QueryAccessKey for call in ret.response.Logs}
+
+    def test_T5820_with_other_AccountId(self):
+        ret = self.a2_r1.oapi.ReadApiLogs()
+        assert len(ret.response.Logs) != 0
+        assert {self.a2_r1.config.account.account_id} == {call.AccountId for call in ret.response.Logs}
+        assert {"ReadTags", "ReadSubnets","ReadApiLogs"} == {call.QueryCallName for call in ret.response.Logs}
+
+    def test_T5821_valid_filter_RequestId_with_other_account(self):
+        ret = self.a1_r1.oapi.ReadApiLogs(Filters={"RequestIds": [self.request_id_a2]})
+        assert len(ret.response.Logs) == 0
