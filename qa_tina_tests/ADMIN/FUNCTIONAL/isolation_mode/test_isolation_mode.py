cf60890bf9c0968ef2c61db00387f3b1c06cd099
lars.smit <lars.smit@outscale.com>
2021-07-28 01:11:29 +0200

    pylint

--- qa_tina_tests/ADMIN/FUNCTIONAL/isolation_mode/test_isolation_mode.py
+++ qa_tina_tests/ADMIN/FUNCTIONAL/isolation_mode/test_isolation_mode.py
@@ -1,7 +1,7 @@
 from time import sleep
 
-from qa_common_tools.ssh import SshTools, OscCommandError
 from qa_sdk_common.exceptions.osc_exceptions import OscApiException
+from qa_common_tools.ssh import SshTools, OscCommandError
 from qa_test_tools.config import config_constants as cfg_constants
 from qa_test_tools.config.configuration import Configuration
 from qa_tina_tools.test_base import OscTinaTest
