7b9bb859a0c9b744a1bb875d34bb64a4cf6b2afa
ismail.elfadli <ismail.elfadli@outscale.com>
2021-08-18 16:27:27 +0200

    add intel.image.modify tests for name and manifest params

---
+++ qa_tina_tests/ADMIN/API/INTEL/image/test_modify.py
@@ -0,0 +1,87 @@
+from qa_sdk_common.exceptions import OscApiException
+from qa_test_tools.misc import assert_error
+from qa_test_tools.test_base import assert_code, known_error
+from qa_tina_tools.test_base import OscTinaTest
+from qa_test_tools.config import config_constants as constants
+
+
+class Test_modify(OscTinaTest):
+
+    @classmethod
+    def setup_class(cls):
+        super(Test_modify, cls).setup_class()
+        cls.image_id = None
+        try:
+            image_id = cls.a1_r1.config.region.get_info(constants.CENTOS_LATEST)
+            cls.image_id = cls.a1_r1.oapi.CreateImage(SourceImageId=image_id,SourceRegionName=cls.a1_r1.config.region.name).response.Image.ImageId
+
+        except Exception as error:
+            try:
+                cls.teardown_class()
+            except Exception as err:
+                raise err
+            finally:
+                raise error
+
+    @classmethod
+    def teardown_class(cls):
+        try:
+            if cls.image_id:
+                cls.a1_r1.oapi.DeleteImage(ImageId=cls.image_id)
+        finally:
+            super(Test_modify, cls).teardown_class()
+
+    def test_T5856_without_params(self):
+        try:
+            self.a1_r1.intel.image.modify()
+            assert False, "Call should not have been successful"
+        except OscApiException as error:
+            assert_error(error, 200, 0, "missing-parameter - Parameter cannot be empty: Owner")
+
+    def test_T5857_with_name(self):
+        name = 'toto'
+        self.a1_r1.intel.image.modify(owner=self.a1_r1.config.account.account_id, image=self.image_id, name="toto")
+        ret = self.a1_r1.intel.image.get(id=self.image_id).response
+        assert ret.result.name == name
+
+    def test_T5858_with_manifest(self):
+        manifest = 'toto'
+        self.a1_r1.intel.image.modify(owner=self.a1_r1.config.account.account_id, image=self.image_id, manifest=manifest)
+        ret = self.a1_r1.intel.image.get(id=self.image_id).response
+        assert ret.result.manifest == manifest
+
+    def test_T5859_with_invalid_name_type(self):
+        try:
+            name = ['foo']
+            self.a1_r1.intel.image.modify(owner=self.a1_r1.config.account.account_id, image=self.image_id, name=name)
+            assert False, 'Call should not have been successful'
+        except OscApiException as error:
+            assert_code(error, 200, "")
+
+    def test_T5860_with_invalid_manifest_type(self):
+        try:
+            manifest = ['foo']
+            self.a1_r1.intel.image.modify(owner=self.a1_r1.config.account.account_id, image=self.image_id, name=manifest)
+            assert False, 'Call should not have been successful'
+        except OscApiException as error:
+            assert_code(error, 200, "")
+
+    def test_T5861_with_empty_manifest(self):
+        try:
+            self.a1_r1.intel.image.modify(owner=self.a1_r1.config.account.account_id, image=self.image_id, manifest="")
+            assert False, 'Call should not have been successful'
+        except OscApiException as error:
+            assert_code(error, 200, "missing-parameter")
+
+    def test_T5862_with_name_and_empty_manifest(self):
+        name = "toto"
+        self.a1_r1.intel.image.modify(owner=self.a1_r1.config.account.account_id, image=self.image_id, name=name, manifest="")
+        ret = self.a1_r1.intel.image.get(id=self.image_id).response
+        assert ret.result.manifest is None
+        assert ret.result.name == name
+        resp = self.a1_r1.oapi.ReadImages(Filters={'ImageIds': [self.image_id]}).response
+        if not hasattr(resp, 'Images'):
+            known_error('TINA-6687', 'ReadImages no more working well')
+        assert False, 'Remove known error'
+        assert resp.Images[0].ImageName == name
+        assert resp.Images[0].FileLocation is None
