5b7d74009c6271373648719c8b55c38c5f6416e9
ghassen.gabsi <ghassen.gabsi@outscale.com>
2021-08-31 09:33:01 +0200

    fix pylint

--- qa_tina_tests/USER/API/OAPI/PublicIp/test_LinkPublicIp.py
+++ qa_tina_tests/USER/API/OAPI/PublicIp/test_LinkPublicIp.py
@@ -1,13 +1,14 @@
 import pytest
 
 from qa_sdk_common.exceptions.osc_exceptions import OscApiException
+from specs import check_oapi_error
 from qa_test_tools.misc import assert_oapi_error, assert_dry_run
 from qa_tina_tools.test_base import OscTinaTest
 from qa_tina_tools.tools.tina.create_tools import create_vpc, create_instances
 from qa_tina_tools.tools.tina.delete_tools import delete_vpc, delete_instances
 from qa_tina_tools.tools.tina.info_keys import SUBNET_ID, SUBNETS, INSTANCE_ID_LIST
 from qa_tina_tools.tools.tina.wait_tools import wait_addresses_state
-from specs import check_oapi_error
+
 
 NUM_STANDARD_EIPS = 1
 NUM_VPC_EIPS = 10
