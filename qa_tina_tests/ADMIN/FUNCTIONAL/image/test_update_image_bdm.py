55f7a7a09c9f910fec9472a8ddcdbbf6f9a2ae41
Nicolas Boissinot <nicolas.boissinot@outscale.com>
2021-09-02 17:34:50 +0200

    update test for PQA-3220

--- qa_tina_tests/ADMIN/FUNCTIONAL/image/test_update_image_bdm.py
+++ qa_tina_tests/ADMIN/FUNCTIONAL/image/test_update_image_bdm.py
@@ -58,11 +58,11 @@ class Test_update_image_bdm(OscTinaTest):
         cls.snapshot_id = None
         super(Test_update_image_bdm, cls).setup_class()
         try:
-            bdm = [{'DeviceName': '/dev/xvdb', 'Bsu': {'DeleteOnVmDeletion': True, 'VolumeSize': SIZE, 'VolumeType': 'standard'}},
-                   {'DeviceName': '/dev/xvdc', 'Bsu': {'DeleteOnVmDeletion': True, 'VolumeSize': SIZE, 'VolumeType': 'io1', 'Iops': IOPS}},
-                   {'DeviceName': '/dev/xvdd', 'Bsu': {'DeleteOnVmDeletion': True, 'VolumeSize': SIZE, 'VolumeType': 'gp2'}}
-                   ]
-            cls.vm_info = oapi.create_Vms(cls.a1_r1, nb=1, bdm=bdm, state='stopped')
+            cls.bdm = [{'DeviceName': '/dev/xvdb', 'Bsu': {'DeleteOnVmDeletion': True, 'VolumeSize': SIZE, 'VolumeType': 'standard'}},
+                       {'DeviceName': '/dev/xvdc', 'Bsu': {'DeleteOnVmDeletion': True, 'VolumeSize': SIZE, 'VolumeType': 'io1', 'Iops': IOPS}},
+                       {'DeviceName': '/dev/xvdd', 'Bsu': {'DeleteOnVmDeletion': True, 'VolumeSize': SIZE, 'VolumeType': 'gp2'}}
+                       ]
+            cls.vm_info = oapi.create_Vms(cls.a1_r1, nb=1, bdm=cls.bdm, state='stopped')
             #oapi.stop_Vms(cls.a1_r1, cls.vm_info[info_keys.VM_IDS], force=False)
         except:
             try:
@@ -103,51 +103,78 @@ class Test_update_image_bdm(OscTinaTest):
 
     def check_image(self, delete_on_vm_termination=True, vol_type=None, vol_size=SIZE, vol_io1_iops=None, snap_id=None):
         vm_info = None
+        vol_list = []
         try:
             vm_info = oapi.create_Vms(self.a1_r1, omi_id=self.image_id)
             resp = self.a1_r1.oapi.ReadVms(Filters={'VmIds': vm_info[info_keys.VM_IDS]}).response
             self.logger.debug(resp.display())
             assert len(resp.Vms) == 1
+            vol_list = [item.Bsu.VolumeId for item in resp.Vms[0].BlockDeviceMappings]
             for item in resp.Vms[0].BlockDeviceMappings:
                 assert item.Bsu.DeleteOnVmDeletion == delete_on_vm_termination
                 vol = self.a1_r1.oapi.ReadVolumes(Filters={'VolumeIds': [item.Bsu.VolumeId]}).response
                 self.logger.debug(vol.display())
                 assert len(vol.Volumes) == 1
-                assert vol.Volumes[0].Size == vol_size
-                assert vol.Volumes[0].SnapshotId
-                assert vol.Volumes[0].VolumeType == vol_type
-                if vol_io1_iops:
-                    assert vol.Volumes[0].Iops == vol_io1_iops
+                if not snap_id or vol.Volumes[0].SnapshotId == snap_id:
+                    assert vol.Volumes[0].Size == vol_size
+                    assert vol.Volumes[0].SnapshotId
+                    if not vol_type:
+                        if vol.Volumes[0].LinkedVolumes[0].DeviceName == '/dev/sda1':
+                            assert vol.Volumes[0].VolumeType == 'standard'
+                        else:
+                            for bsu in self.bdm:
+                                if bsu['DeviceName'] == vol.Volumes[0].LinkedVolumes[0].DeviceName:
+                                    assert vol.Volumes[0].VolumeType == bsu['Bsu']['VolumeType']
+                    else:
+                        assert vol.Volumes[0].VolumeType == vol_type
+                    if vol_io1_iops:
+                        assert vol.Volumes[0].Iops == vol_io1_iops
+                    else:
+                        assert not hasattr(vol.Volumes[0], "Iops")
                 else:
-                    assert not hasattr(vol.Volumes[0], "Iops")
-
-
+                    assert vol.Volumes[0].Size == SIZE
+                    assert vol.Volumes[0].SnapshotId
+                    if vol.Volumes[0].LinkedVolumes[0].DeviceName == '/dev/sda1':
+                        assert vol.Volumes[0].VolumeType == 'standard'
+                    else:
+                        for bsu in self.bdm:
+                            if bsu['DeviceName'] == vol.Volumes[0].LinkedVolumes[0].DeviceName:
+                                assert vol.Volumes[0].VolumeType == bsu['Bsu']['VolumeType']
+                    if vol.Volumes[0].VolumeType == 'io1':
+                        assert vol.Volumes[0].Iops == IOPS
         finally:
             if vm_info:
                 oapi.delete_Vms(self.a1_r1, vm_info)
+            if not delete_on_vm_termination:
+                for vol in vol_list:
+                    self.a1_r1.oapi.DeleteVolume(VolumeId=vol)
+                    wait.wait_Volumes_state(self.a1_r1, [vol], cleanup=True)
 
-    # sample successful test
-    def test_T0000_modified_size(self):
+    def test_T5926_modified_size(self):
         self.a1_r1.intel.image.update_bdm(owner=self.a1_r1.config.account.account_id, image_id=self.image_id, size=(SIZE + 1) * 2**30)
         try:
-            self.check_image()
+            self.check_image(vol_size=SIZE + 1)
             assert False, 'Remove known error'
         except OscApiException:
             known_error('TINA-6700', 'Iops value is set to None after an intel.update_bdm')
 
-    def test_T0000_modified_type_to_std(self):
+    def test_T5927_modified_type_to_std(self):
         self.a1_r1.intel.image.update_bdm(owner=self.a1_r1.config.account.account_id, image_id=self.image_id, volume_type="standard")
         self.check_image(vol_type="standard")
 
-    def test_T0000_modified_type_to_io1(self):
+    def test_T5928_modified_type_to_io1(self):
         self.a1_r1.intel.image.update_bdm(owner=self.a1_r1.config.account.account_id, image_id=self.image_id, volume_type='io1', iops=IOPS)
         self.check_image(vol_type="io1", vol_io1_iops=IOPS)
 
-    def test_T0000_modified_delete_on_termination(self):
+    def test_T5929_modified_delete_on_termination(self):
         self.a1_r1.intel.image.update_bdm(owner=self.a1_r1.config.account.account_id, image_id=self.image_id, delete_on_termination=False)
-        self.check_image(delete_on_vm_termination=False)
+        try:
+            self.check_image(delete_on_vm_termination=False)
+            assert False, 'Remove known error'
+        except OscApiException:
+            known_error('TINA-6700', 'Iops value is set to None after an intel.update_bdm')
 
-    def test_T0000_modified_only_one_vol(self):
+    def test_T5930_modified_only_one_vol(self):
         self.a1_r1.intel.image.update_bdm(owner=self.a1_r1.config.account.account_id, image_id=self.image_id, snapshot_id=self.snapshot_id,
                                           size=(SIZE + 1) * 2**30)
         self.check_image(vol_size=SIZE + 1, snap_id=self.snapshot_id)
