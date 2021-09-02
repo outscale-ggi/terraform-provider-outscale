ec96e232f19dee7fee24f864d8aba1c3c264956f
taher.kriaa <taher.kriaa@outscale.com>
2021-09-02 17:13:05 +0200

    update vpn tests

--- qa_tina_tests/USER/FUNCTIONAL/FCU/VPN_Connections/Dynamic/test_vpn_dynamic.py
+++ qa_tina_tests/USER/FUNCTIONAL/FCU/VPN_Connections/Dynamic/test_vpn_dynamic.py
@@ -8,11 +8,10 @@ from qa_tina_tests.USER.FUNCTIONAL.FCU.VPN_Connections.vpn import Vpn
 @pytest.mark.region_internet
 class Test_vpn_dynamic(Vpn):
 
-    @pytest.mark.tag_redwire
     @pytest.mark.centos7
     def test_T126_test_vpn_dynamic(self):
         self.exec_test_vpn(static=False, racoon=True, default_rtb=True)
 
-    @pytest.mark.centos7
+    @pytest.mark.tag_redwire
     def test_T5141_test_vpn_dynamic_strongswan(self):
         self.exec_test_vpn(static=False, racoon=False, default_rtb=True)
