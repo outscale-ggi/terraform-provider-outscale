4d1963a87e4379ada50f18308a53095de4320b71
taher.kriaa <taher.kriaa@outscale.com>
2021-09-03 18:25:28 +0200

    fix pylint

--- qa_tina_tests/USER/API/OAPI/VpnConnection/test_UpdateVpnConnection.py
+++ qa_tina_tests/USER/API/OAPI/VpnConnection/test_UpdateVpnConnection.py
@@ -1,7 +1,7 @@
 
+from qa_sdk_common.exceptions.osc_exceptions import OscApiException
 from qa_tina_tools.test_base import OscTinaTest
 from qa_tina_tools.tina import oapi, wait
-from qa_sdk_common.exceptions.osc_exceptions import OscApiException
 from qa_test_tools.test_base import known_error
 from qa_test_tools.misc import assert_oapi_error, id_generator
 from qa_test_tools.config.configuration import Configuration
