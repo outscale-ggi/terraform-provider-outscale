382367112d3fd24e64b0bd038dc807de11ca12af
ismail.elfadli <ismail.elfadli@outscale.com>
2021-09-14 16:24:50 +0200

    remove known err tag issue

--- qa_tina_tests/USER/API/FCU/Volumes/test_DescribeVolumes.py
+++ qa_tina_tests/USER/API/FCU/Volumes/test_DescribeVolumes.py
@@ -109,9 +109,6 @@ class Test_DescribeVolumes(OscTinaTest):
 
     def test_T5947_filter_by_tag_key_and_value_wildcard(self):
         ret = self.a1_r1.fcu.DescribeVolumes(Filter=[{'Name': 'tag:key2', 'Value': '*'}])
-        if ret.response.volumeSet is None:
-            known_error('TINA-6737', 'tag None')
-        assert False, 'Remove known error'
         assert len(ret.response.volumeSet) == 2
 
     def test_T5948_one_tag_key_multiple_tag_values(self):
