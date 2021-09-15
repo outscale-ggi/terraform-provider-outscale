3b6b02fbaa8a6e30cacd16bd4c2f3875c53e35c3
ghassen.gabsi <ghassen.gabsi@outscale.com>
2021-09-15 18:46:00 +0200

    fix

--- tests/qa_provider_oapi/test_provider_oapi.py
+++ tests/qa_provider_oapi/test_provider_oapi.py
@@ -89,7 +89,7 @@ terraform {
     required_providers {
         outscale = {
             source = "outscale-dev/outscale"
-            version = var.version
+            version = var.version_plugin
         }
     }
 }
@@ -107,7 +107,7 @@ variable "account_id" {}
 variable "access_key_id" {}
 variable "secret_key_id" {}
 variable "region" {}
-variable "version" {}
+variable "version_plugin" {}
 
 # resources configuration
 variable "image_id" {}
