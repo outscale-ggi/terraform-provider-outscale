9f445619da3949a681318a2430332c7784f47ec6
taher.kriaa <taher.kriaa@outscale.com>
2021-09-17 12:28:36 +0200

    fix

--- qa_tina_tests/USER/API/OAPI/VpnConnection/test_ReadVpnConnections.py
+++ qa_tina_tests/USER/API/OAPI/VpnConnection/test_ReadVpnConnections.py
@@ -17,6 +17,7 @@ class Test_ReadVpnConnections(VpnConnection):
         cls.vgw_ids = []
         cls.QUOTAS = {'vpg_limit', 6}
         cls.QUOTAS = {'cgw_limit', 6}
+        cls.QUOTAS = {'vpnc_limit', 6}
         super(Test_ReadVpnConnections, cls).setup_class()
         try:
             cls.a1_r1.oapi.CreateVpnConnectionRoute(VpnConnectionId=cls.vpn_id, DestinationIpRange='172.13.1.4/24')
