8de568241ef5c9bc4f102c5b77c53fa75b1a4ee1
Nicolas Boissinot <nicolas.boissinot@outscale.com>
2021-09-17 10:39:02 +0200

    fix pylint

--- qa_tina_tests/ADMIN/API/INTEL/virtual_private_gateway/test_delete_recursive.py
+++ qa_tina_tests/ADMIN/API/INTEL/virtual_private_gateway/test_delete_recursive.py
@@ -1,11 +1,8 @@
-from qa_sdk_common.exceptions.osc_exceptions import OscApiException
 from qa_tina_tools.test_base import OscTinaTest
 from qa_tina_tools.tina.wait import wait_VpnConnections_state
 from qa_tina_tools.tools.tina.create_tools import get_random_public_ip
 from qa_tina_tools.tools.tina.wait_tools import wait_customer_gateways_state, wait_vpn_gateways_state, wait_vpn_gateways_attachment_state, \
     wait_vpn_connections_state
-from qa_test_tools.misc import assert_error
-from qa_test_tools.test_base import known_error
 
 
 class Test_delete_recursive(OscTinaTest):
