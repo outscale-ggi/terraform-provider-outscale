c1c745c77ab8f09e115e03059e375248c8b6fc8c
ismail.elfadli <ismail.elfadli@outscale.com>
2021-09-22 13:30:12 +0200

    add known error OPS-14319 to Allocate, Confirm, Create PVI and Describe VI

--- qa_tina_tests/USER/API/DirectLink/Virtual_Interfaces/test_ConfirmPrivateVirtualInterface.py
+++ qa_tina_tests/USER/API/DirectLink/Virtual_Interfaces/test_ConfirmPrivateVirtualInterface.py
@@ -4,6 +4,7 @@ import time
 import pytest
 
 from qa_test_tools.misc import id_generator
+from qa_test_tools.test_base import known_error
 from qa_tina_tools.test_base import OscTinaTest
 from qa_tina_tools.tools.tina import wait_tools
 
@@ -16,17 +17,35 @@ class Test_ConfirmPrivateVirtualInterface(OscTinaTest):
     def setup_class(cls):
         cls.conn_id = None
         cls.quotas = {'dl_connection_limit': 1, 'dl_interface_limit': 1}
+        cls.known_error = False
         super(Test_ConfirmPrivateVirtualInterface, cls).setup_class()
-        ret = cls.a1_r1.directlink.DescribeLocations()
-        cls.location_code = ret.response.locations[0].locationCode
+        try:
+            ret = cls.a1_r1.directlink.DescribeLocations()
+            if cls.a1_r1.config.region.name == 'in-west-2':
+                if len(ret.response.locations) == 0:
+                    cls.known_error = True
+                    return
+                assert False, 'remove known error'
+            cls.location_code = ret.response.locations[0].locationCode
+        except Exception as error1:
+            try:
+                cls.teardown_class()
+            except Exception as error2:
+                raise error2
+            finally:
+                raise error1
 
     def setup_method(self, method):
         self.conn_id = None
+        if self.known_error:
+            return
         OscTinaTest.setup_method(self, method)
         ret = self.a1_r1.directlink.CreateConnection(location=self.location_code, bandwidth='1Gbps', connectionName=id_generator(prefix='dl_'))
         self.conn_id = ret.response.connectionId
 
     def teardown_method(self, method):
+        if self.known_error:
+            known_error('OPS-14319', 'NEW IN2 : no Directlink on in2')
         try:
             if self.conn_id:
                 self.a1_r1.intel.dl.connection.delete(owner=self.a1_r1.config.account.account_id, connection_id=self.conn_id)
@@ -34,6 +53,8 @@ class Test_ConfirmPrivateVirtualInterface(OscTinaTest):
             OscTinaTest.teardown_method(self, method)
 
     def test_T1915_required_param(self):
+        if self.known_error:
+            known_error('OPS-14319', 'NEW IN2 : no Directlink on in2')
         allocation = {'asn': 11111, 'virtualInterfaceName': 'test', 'vlan': 2}
         alloc_info = None
         vgw_id = None
@@ -61,6 +82,8 @@ class Test_ConfirmPrivateVirtualInterface(OscTinaTest):
                 wait_tools.wait_vpn_gateways_state(self.a1_r1, [vgw_id], state='deleted')
 
     def test_T5738_with_extra_param(self):
+        if self.known_error:
+            known_error('OPS-14319', 'NEW IN2 : no Directlink on in2')
         allocation = {'asn': 11111, 'virtualInterfaceName': 'test', 'vlan': 2}
         alloc_info = None
         vgw_id = None
--- qa_tina_tests/USER/API/DirectLink/Virtual_Interfaces/test_CreatePrivateVirtualInterface.py
+++ qa_tina_tests/USER/API/DirectLink/Virtual_Interfaces/test_CreatePrivateVirtualInterface.py
@@ -3,6 +3,7 @@ import pytest
 
 from qa_sdk_common.exceptions.osc_exceptions import OscApiException
 from qa_test_tools.misc import assert_error, id_generator
+from qa_test_tools.test_base import known_error
 from qa_tina_tools.test_base import OscTinaTest
 from qa_tina_tools.tools.tina import wait_tools
 
@@ -15,11 +16,27 @@ class Test_CreatePrivateVirtualInterface(OscTinaTest):
     def setup_class(cls):
         cls.conn_id = None
         cls.quotas = {'dl_connection_limit': 10, 'dl_interface_limit': 10}
+        cls.known_error = False
         super(Test_CreatePrivateVirtualInterface, cls).setup_class()
-        ret = cls.a1_r1.directlink.DescribeLocations()
-        cls.location_code = ret.response.locations[0].locationCode
+        try:
+            ret = cls.a1_r1.directlink.DescribeLocations()
+            if cls.a1_r1.config.region.name == 'in-west-2':
+                if len(ret.response.locations) == 0:
+                    cls.known_error = True
+                    return
+                assert False, 'remove known error'
+            cls.location_code = ret.response.locations[0].locationCode
+        except Exception as error1:
+            try:
+                cls.teardown_class()
+            except Exception as error2:
+                raise error2
+            finally:
+                raise error1
 
     def setup_method(self, method):
+        if self.known_error:
+            return
         self.conn_id = None
         OscTinaTest.setup_method(self, method)
         ret = self.a1_r1.directlink.CreateConnection(location=self.location_code, bandwidth='1Gbps', connectionName=id_generator(prefix='dl_'))
@@ -34,6 +51,8 @@ class Test_CreatePrivateVirtualInterface(OscTinaTest):
             OscTinaTest.teardown_method(self, method)
 
     def test_T4658_invalid_connection_state(self):
+        if self.known_error:
+            known_error('OPS-14319', 'NEW IN2 : no Directlink on in2')
         interface_info = None
         interface = {'asn': 11111, 'virtualInterfaceName': 'test', 'vlan': 2}
         try:
@@ -46,6 +65,8 @@ class Test_CreatePrivateVirtualInterface(OscTinaTest):
                 self.a1_r1.directlink.DeleteVirtualInterface(virtualInterfaceId=interface_info.response.virtualInterfaceId)
 
     def test_T1910_required_param(self):
+        if self.known_error:
+            known_error('OPS-14319', 'NEW IN2 : no Directlink on in2')
         interface = {'asn': 11111, 'virtualInterfaceName': 'test', 'vlan': 2}
         interface_info = None
         try:
@@ -71,6 +92,8 @@ class Test_CreatePrivateVirtualInterface(OscTinaTest):
                 self.a1_r1.directlink.DeleteVirtualInterface(virtualInterfaceId=interface_info.response.virtualInterfaceId)
 
     def test_T4659_without_interface(self):
+        if self.known_error:
+            known_error('OPS-14319', 'NEW IN2 : no Directlink on in2')
         interface_info = None
         try:
             self.a1_r1.intel.dl.connection.activate(owner=self.a1_r1.config.account.account_id, connection_id=self.conn_id)
@@ -83,6 +106,8 @@ class Test_CreatePrivateVirtualInterface(OscTinaTest):
                 self.a1_r1.directlink.DeleteVirtualInterface(virtualInterfaceId=interface_info.response.virtualInterfaceId)
 
     def test_T4660_without_connectionId(self):
+        if self.known_error:
+            known_error('OPS-14319', 'NEW IN2 : no Directlink on in2')
         interface_info = None
         try:
             self.a1_r1.intel.dl.connection.activate(owner=self.a1_r1.config.account.account_id, connection_id=self.conn_id)
@@ -95,6 +120,8 @@ class Test_CreatePrivateVirtualInterface(OscTinaTest):
                 self.a1_r1.directlink.DeleteVirtualInterface(virtualInterfaceId=interface_info.response.virtualInterfaceId)
 
     def test_T4661_invalid_vlan(self):
+        if self.known_error:
+            known_error('OPS-14319', 'NEW IN2 : no Directlink on in2')
         interface_info = None
         interface = {'asn': '11111', 'virtualInterfaceName': 'test', 'vlan': 's'}
         try:
@@ -110,6 +137,8 @@ class Test_CreatePrivateVirtualInterface(OscTinaTest):
                 self.a1_r1.directlink.DeleteVirtualInterface(virtualInterfaceId=interface_info.response.virtualInterfaceId)
 
     def test_T4662_invalid_asn(self):
+        if self.known_error:
+            known_error('OPS-14319', 'NEW IN2 : no Directlink on in2')
         interface_info = None
         interface = {'asn': '11111', 'virtualInterfaceName': 'test', 'vlan': 1}
         try:
@@ -125,6 +154,8 @@ class Test_CreatePrivateVirtualInterface(OscTinaTest):
                 self.a1_r1.directlink.DeleteVirtualInterface(virtualInterfaceId=interface_info.response.virtualInterfaceId)
 
     def test_T4663_invalid_virtualInterfaceName(self):
+        if self.known_error:
+            known_error('OPS-14319', 'NEW IN2 : no Directlink on in2')
         interface_info = None
         interface = {'asn': '11111', 'virtualInterfaceName': 123, 'vlan': 1}
         try:
@@ -140,6 +171,8 @@ class Test_CreatePrivateVirtualInterface(OscTinaTest):
                 self.a1_r1.directlink.DeleteVirtualInterface(virtualInterfaceId=interface_info.response.virtualInterfaceId)
 
     def test_T5367_with_existing_virtual_interface(self):
+        if self.known_error:
+            known_error('OPS-14319', 'NEW IN2 : no Directlink on in2')
         interface_info1 = None
         interface = {'asn': 11111, 'virtualInterfaceName': 'test', 'vlan': 2}
         try:
@@ -161,6 +194,8 @@ class Test_CreatePrivateVirtualInterface(OscTinaTest):
                 self.a1_r1.directlink.DeleteVirtualInterface(virtualInterfaceId=interface_info1.response.virtualInterfaceId)
 
     def test_T5739_with_extra_param(self):
+        if self.known_error:
+            known_error('OPS-14319', 'NEW IN2 : no Directlink on in2')
         interface = {'asn': 11111, 'virtualInterfaceName': 'test', 'vlan': 2}
         interface_info = None
         try:
