96fd071f68c5ff9f4463b707195d15dc24ee973d
lars.smit <lars.smit@oustcale.com>
2021-10-20 12:15:38 +0200

    test

--- qa_tina_tests/USER/API/OAPI/LoadBalancer/test_CreateLoadBalancer.py
+++ qa_tina_tests/USER/API/OAPI/LoadBalancer/test_CreateLoadBalancer.py
@@ -688,7 +688,7 @@ class Test_CreateLoadBalancer(LoadBalancer):
                         ignored_keys=["DnsName"])
         if name:
             try:
-                self.a1_r1.oapi.DeleteLoadBalancer(LoadBalancerName=name)
+                # self.a1_r1.oapi.DeleteLoadBalancer(LoadBalancerName=name)
                 delete_lbu(self.a1_r1, name)
                 cleanup_load_balancers(self.a1_r1,  filters={'LoadBalancerNames': name}, force=True)
             except:
