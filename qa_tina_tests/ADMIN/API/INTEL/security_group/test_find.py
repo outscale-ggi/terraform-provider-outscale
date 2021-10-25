bf6a56ff8554f1f6b645cfa209f0589681e5d7ab
ghassen.gabsi <ghassen.gabsi@outscale.com>
2021-10-25 09:25:22 +0200

    update

--- qa_tina_tests/ADMIN/API/INTEL/security_group/test_find.py
+++ qa_tina_tests/ADMIN/API/INTEL/security_group/test_find.py
@@ -1,3 +1,4 @@
+from qa_test_tools.config.configuration import Configuration
 from qa_tina_tools.test_base import OscTinaTest
 
 
@@ -9,12 +10,15 @@ class Test_find(OscTinaTest):
             ret = self.a1_r1.intel.security_group.create(owner=self.a1_r1.config.account.account_id, name='test', description='test')
             group_id = ret.response.result.id
             print(ret.response.result.display())
-            TAG1 = {"key1": "value1"}
-            TAG2 = {"key2": "value2"}
-            TAGS = dict(TAG1, **TAG2)
-            self.a1_r1.intel.api.tag.create(owner=self.a1_r1.config.account.account_id, resource_id=[group_id], tags=TAGS)
-            assert len(self.a1_r1.intel.api.tag.find(key="key1")) == 1
-            assert len(self.a1_r1.intel.api.tag.find(valeur="key1")) == 1
+            tag1 = {"key1": "value1"}
+            tag2 = {"key2": "value2"}
+            tags = dict(tag1, **tag2)
+            self.a1_r1.intel.tag.create(owner=self.a1_r1.config.account.account_id, resource_id=[group_id], tags=tags)
+            assert len(self.a1_r1.intel.tag.find(key=["key1"]).response.result) == 1
+            assert len(self.a1_r1.intel.tag.find(value=["value2"]).response.result) == 1
+            assert len(self.a1_r1.intel.tag.find().response.result) >= 1
+            assert len(self.a1_r1.intel.tag.find(key=['key1', 'key2']).response.result) == 2
+            assert len(self.a1_r1.intel.tag.find(value=['value1', 'value2']).response.result) == 2
         except Exception as error:
             raise error
         finally:
