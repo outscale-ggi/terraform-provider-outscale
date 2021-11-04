e6dda3a802e4e44a787ee2154f6edc33281c30e3
taher.kriaa <taher.kriaa@outscale.com>
2021-11-04 11:55:33 +0100

    new vpn functional test

---
+++ qa_tina_tests/USER/FUNCTIONAL/FCU/VPN_Connections/UpdateVpnConnection/test_update_vpn_connection.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+
+from qa_tina_tests.USER.FUNCTIONAL.FCU.VPN_Connections.vpn import Vpn
+from qa_test_tools.misc import id_generator
+
+
+@pytest.mark.region_internet
+class Test_update_vpn_connection(Vpn):
+
+    def test_T6103_with_presharedkey(self):
+        presharedkey = id_generator(size=64)
+        self.exec_test_vpn(static=False, racoon=False, default_rtb=True, presharedkey=presharedkey)
