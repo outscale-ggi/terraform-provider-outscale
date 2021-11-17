243079c33ac19de8b8ec83677f6173ae9e36d94c
ismail.elfadli <ismail.elfadli@outscale.com>
2021-11-17 14:07:42 +0100

    correct T5887_static_website_bucket, right region

--- qa_tina_tests/USER/FUNCTIONAL/OOS/test_oos.py
+++ qa_tina_tests/USER/FUNCTIONAL/OOS/test_oos.py
@@ -127,11 +127,12 @@ class Test_oos(OscTinaTest):
                 'https://{}.{}-website.{}.outscale.com'.format(bucket_name, service, self.a1_r1.config.region.name),
                 verify=self.a1_r1.config.region.get_info(constants.VALIDATE_CERTS)
             )
-            if "InvalidURI" in response.text:
-                known_error('OPS-14142', "OOS website bucket issue on IN1")
-            else:
-                assert False, 'Remove known error'
-            assert "Hello World it's the index !" in response.text
+            if self.a1_r1.config.region.name == 'in-west-1':
+                if "InvalidURI" in response.text:
+                    known_error('OPS-14142', "OOS website bucket issue on IN1")
+                else:
+                    assert False, 'Remove known error'
+                assert "Hello World it's the index !" in response.text
         finally:
             errors = []
             if obj_name:
