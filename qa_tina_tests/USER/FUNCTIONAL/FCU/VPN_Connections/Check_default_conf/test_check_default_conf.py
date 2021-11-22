f75bd201a34be50753ecc08cb000adeff507fd54
taher.kriaa <taher.kriaa@outscale.com>
2021-11-22 16:41:09 +0100

    new vpn tests

---
+++ qa_tina_tests/USER/FUNCTIONAL/FCU/VPN_Connections/Check_default_conf/test_check_default_conf.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+
+from qa_tina_tests.USER.FUNCTIONAL.FCU.VPN_Connections.vpn import Vpn
+
+
+class Test_check_default_conf(Vpn):
+
+    def test_T6117_check_strongswan_conf(self):
+        self.exec_test_vpn(static=True, racoon=False, default_rtb=True, check_conf=True)
--- qa_tina_tests/USER/FUNCTIONAL/FCU/VPN_Connections/UpdateVpnConnection/test_update_vpn_connection.py
+++ qa_tina_tests/USER/FUNCTIONAL/FCU/VPN_Connections/UpdateVpnConnection/test_update_vpn_connection.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import ipaddress
 import pytest
 
 from qa_tina_tests.USER.FUNCTIONAL.FCU.VPN_Connections.vpn import Vpn
@@ -12,3 +13,11 @@ class Test_update_vpn_connection(Vpn):
     def test_T6103_with_presharedkey(self):
         presharedkey = id_generator(size=64)
         self.exec_test_vpn(static=False, racoon=False, default_rtb=True, presharedkey=presharedkey)
+
+    def test_T6115_with_tunnelinsideiprange(self):
+        self.exec_test_vpn(static=False, racoon=False, default_rtb=True, tunnelinsideiprange=ipaddress.ip_address("169.254.254.8"))
+
+    def test_T6116_with_presharedkey_and_tunnelinsideiprange(self):
+        presharedkey = id_generator(size=64)
+        self.exec_test_vpn(static=False, racoon=False, default_rtb=True, presharedkey=presharedkey,
+                           tunnelinsideiprange=ipaddress.ip_address("169.254.254.8"))
