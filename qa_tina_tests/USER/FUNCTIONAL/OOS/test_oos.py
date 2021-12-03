7ee90b8b4b0677d503c627b83d495d54c7c7e84d
ismail.elfadli <ismail.elfadli@outscale.com>
2021-12-03 09:58:42 +0000

    Update test_oos.py

--- qa_tina_tests/USER/FUNCTIONAL/OOS/test_oos.py
+++ qa_tina_tests/USER/FUNCTIONAL/OOS/test_oos.py
@@ -9,6 +9,7 @@ import requests
 
 from qa_test_tools import misc
 from qa_test_tools.config import config_constants as constants
+from qa_test_tools.test_base import known_error
 from qa_tina_tools.test_base import OscTinaTest
 from qa_tina_tools.tina import storage
 
